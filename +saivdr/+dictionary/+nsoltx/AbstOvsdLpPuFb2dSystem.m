736e1694f4c77be43bb02f51d9af8189fa9f8164
x34j <spq42z79@sand.ocn.ne.jp>
2016-04-29 00:21:14 +0900

    エラー修正

--- +saivdr/+dictionary/+nsoltx/AbstOvsdLpPuFb2dSystem.m
+++ +saivdr/+dictionary/+nsoltx/AbstOvsdLpPuFb2dSystem.m
@@ -15,7 +15,7 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
     %                8050 2-no-cho Ikarashi, Nishi-ku,
     %                Niigata, 950-2181, JAPAN
     %
-    % LinedIn: http://www.linkedin.com/pub/shogo-muramatsu/4b/b08/627    
+    % LinedIn: http://www.linkedin.com/pub/shogo-muramatsu/4b/b08/627
     %
 
     properties (Nontunable)
@@ -24,7 +24,7 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
         NumberOfChannels = [];
         OutputMode = 'Coefficients'
     end
-    
+
     properties (Hidden, Transient)
         OutputModeSet = ...
             matlab.system.StringSet({...
@@ -33,18 +33,18 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
             'AnalysisFilterAt',...
             'SynthesisFilters',...
             'ParameterMatrixSet',...
-            'Char'});            
+            'Char'});
     end
-    
+
     properties (Hidden)
         Angles = 0;
-        Mus    = 1;   
+        Mus    = 1;
     end
 
     properties (GetAccess = public, SetAccess = protected)
         ParameterMatrixSet
     end
-        
+
     properties (Access = protected)
         Coefficients
     end
@@ -52,23 +52,23 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
     properties(Access = protected, Logical)
         mexFlag = false
     end
-    
+
     methods (Access = protected, Abstract = true)
         value = getAnalysisFilterBank_(obj)
         updateParameterMatrixSet_(obj)
         updateAngles_(obj)
         updateMus_(obj)
     end
-    
-    methods 
+
+    methods
         function obj = AbstOvsdLpPuFb2dSystem(varargin)
             setProperties(obj,nargin,varargin{:});
         end
-        
+
         function atmimshow(obj)
             % Show Atomic Images
             updateParameterMatrixSet_(obj);
-            obj.mexFlag = false;            
+            obj.mexFlag = false;
             H = getAnalysisFilterBank_(obj);
             for ib=1:sum(obj.NumberOfChannels)
                 subplot(2,obj.NumberOfChannels(1),ib);
@@ -77,7 +77,7 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
         end
 
     end
-    
+
     methods (Access = protected)
 
         function s = saveObjectImpl(obj)
@@ -86,16 +86,16 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
             s.Coefficients = obj.Coefficients;
             s.mexFlag = obj.mexFlag;
         end
-        
+
         function loadObjectImpl(obj,s,wasLocked)
             import saivdr.dictionary.utility.ParameterMatrixSet
-            obj.mexFlag = s.mexFlag;            
+            obj.mexFlag = s.mexFlag;
             obj.Coefficients = s.Coefficients;
             loadObjectImpl@matlab.System(obj,s,wasLocked);
             %
             obj.ParameterMatrixSet = matlab.System.loadObject(s.ParameterMatrixSet);
-        end        
-        
+        end
+
         function validatePropertiesImpl(obj)
             id = 'SaivDr:IllegalPropertyException';
             if prod(obj.DecimationFactor) > sum(obj.NumberOfChannels)
@@ -103,13 +103,13 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
                     id);
             end
         end
-        
+
         function resetImpl(obj)
             obj.mexFlag = false;
         end
-        
+
         function output = stepImpl(obj,varargin)
-            
+
             if ~isempty(varargin{1})
                 obj.Angles = varargin{1};
                 updateAngles_(obj);
@@ -118,25 +118,25 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
                 obj.Mus = varargin{2};
                 updateMus_(obj);
             end
-            
+
             updateParameterMatrixSet_(obj);
             updateCoefficients_(obj);
-            
-            if strcmp(obj.OutputMode,'Coefficients') 
+
+            if strcmp(obj.OutputMode,'Coefficients')
                 output = obj.Coefficients;
             elseif strcmp(obj.OutputMode,'ParameterMatrixSet')
                 output = clone(obj.ParameterMatrixSet);
-            elseif strcmp(obj.OutputMode,'AnalysisFilterAt')     
+            elseif strcmp(obj.OutputMode,'AnalysisFilterAt')
                 idx = varargin{3};
                 H = getAnalysisFilterBank_(obj);
                 output = H(:,:,idx);
-            elseif strcmp(obj.OutputMode,'AnalysisFilters')     
+            elseif strcmp(obj.OutputMode,'AnalysisFilters')
                 output = getAnalysisFilterBank_(obj);
-            elseif strcmp(obj.OutputMode,'SynthesisFilterAt')     
+            elseif strcmp(obj.OutputMode,'SynthesisFilterAt')
                 idx = varargin{3};
                 H = getAnalysisFilterBank_(obj);
                 output = rot90(H(:,:,idx),2);
-            elseif strcmp(obj.OutputMode,'SynthesisFilters')     
+            elseif strcmp(obj.OutputMode,'SynthesisFilters')
                 H = getAnalysisFilterBank_(obj);          
                 H = flip(H,1);
                 output = flip(H,2);
@@ -144,27 +144,27 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
                 output = [];
             end
         end
-        
+
         function N = getNumInputsImpl(obj)
             if strcmp(obj.OutputMode,'Coefficients') || ...
                     strcmp(obj.OutputMode,'Char') || ...
                     strcmp(obj.OutputMode,'ParameterMatrixSet') || ...
                     strcmp(obj.OutputMode,'AnalysisFilters') || ...
-                    strcmp(obj.OutputMode,'SynthesisFilters') 
+                    strcmp(obj.OutputMode,'SynthesisFilters')
                 N = 2;
             else
                 N = 3;
             end
         end
-        
+
         function N = getNumOutputsImpl(~)
             N = 1;
-        end        
-        
+        end
+
         function updateCoefficients_(obj)
             import saivdr.dictionary.utility.Direction
             decX = obj.DecimationFactor(Direction.HORIZONTAL);
-            decY = obj.DecimationFactor(Direction.VERTICAL);            
+            decY = obj.DecimationFactor(Direction.VERTICAL);
             ordX = obj.PolyPhaseOrder(Direction.HORIZONTAL);
             ordY = obj.PolyPhaseOrder(Direction.VERTICAL);
             nChs = obj.NumberOfChannels;
@@ -187,7 +187,7 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
             end
             obj.Coefficients = coefs;
         end
-        
+
         function value = getMatrixE0_(obj)
             import saivdr.dictionary.utility.Direction
             nRows = obj.DecimationFactor(Direction.VERTICAL);
@@ -234,9 +234,9 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
                 end
             end
             value = coefs;
-            
+
         end
-        
+
         function value = permuteCoefs_(~,arr_,phs_)
             len_ = size(arr_,2)/phs_;
             value = zeros(size(arr_));
@@ -244,7 +244,7 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
                 value(:,idx*len_+1:(idx+1)*len_) = arr_(:,idx+1:phs_:end);
             end
         end
-        
+
         function value = ipermuteCoefs_(~,arr_,phs_)
             len_ = size(arr_,2)/phs_;
             value = zeros(size(arr_));
@@ -252,7 +252,7 @@ classdef AbstOvsdLpPuFb2dSystem < matlab.System %#codegen
                 value(:,idx+1:phs_:end) = arr_(:,idx*len_+1:(idx+1)*len_);
             end
         end
-        
+
     end
-    
+
 end
