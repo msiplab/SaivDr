4083d2a1fcda8dc5774589bb7d9bcda010303134
x34j <spq42z79@sand.ocn.ne.jp>
2016-09-23 17:25:16 +0900

    微調整

--- DictionaryLearning.m
+++ DictionaryLearning.m
@@ -3,8 +3,9 @@ classdef DictionaryLearning
     properties
         orgImg
         nsolt
-        NumberOfCoefs = 20000
-        Count = 10
+        NumberOfCoefs = 10000
+        StageCount
+        MaxStageCount = 10
         MaxFunctionEvaluations = 100000
         MaxIterations = 1000
         Angles = []
@@ -37,16 +38,20 @@ classdef DictionaryLearning
         function update(obj)
             
             angs = get(obj.nsolt,'Angles');
-            obj.Angles = zeros(length(angs),obj.Count);
+            obj.Angles = zeros(length(angs),obj.MaxStageCount);
             
             opt = optimoptions(@fminunc,...
                 'Display','iter-detailed',...
                 'Algorithm','quasi-newton',...
+                'UseParallel',true,...
                 'GradObj','off',...
+                'DiffMaxChange',2*pi,...
+                'StepTolerance',10e-8,...
                 'MaxFunctionEvaluations',obj.MaxFunctionEvaluations,...
                 'MaxIterations',obj.MaxIterations);
             
-            for idx = 1:obj.Count
+            for idx = 1:obj.MaxStageCount
+                obj.StageCount = idx;
                 % coefficients optimization
                 import saivdr.dictionary.nsoltx.*
                 import saivdr.sparserep.*
@@ -77,20 +82,25 @@ classdef DictionaryLearning
                 diff = obj.orgImg - step(synthesizer,coefvec,scales);
                 value = sum(abs(diff(:)).^2)/numel(diff);
             end
-            
             func = @objFunc;
-            
-            %     grad = zeros(size(angs));
-            %     for idx = 1:length(angs)
-            %         dangs = angs;
-            %         dangs(idx) = angs(idx)+pi/2;
-            %         release(nsolt);
-            %         set(nsolt,'Angles',dangs);
-            %         dsynth = saivdr.dictionary.nsoltx.NsoltSynthesis2dSystem('LpPuFb2d',nsolt);
-            %
-            %         tmp1 = conj(diff).*step(dsynth,coefs,scales);
-            %         grad(idx) = -2*real(sum(tmp1(:)))/numel(tmp1);
-            %     end
         end
+        
+%         function func = getObjFuncWithGrad(obj,coefvec,scales)
+%             function [value, grad] = objFunc(angs)
+%                 value = getObjFunc(obj,coefvec,scales);
+%                 grad = zeros(size(angs));
+%                 for idx = 1:length(angs)
+%                     dangs = angs;
+%                     dangs(idx) = angs(idx)+pi/2;
+%                     release(obj.nsolt);
+%                     set(obj.nsolt,'Angles',dangs);
+%                     dsynth = saivdr.dictionary.nsoltx.NsoltSynthesis2dSystem('LpPuFb2d',obj.nsolt);
+%                     
+%                     tmp1 = conj(diff).*step(dsynth,coefs,scales);
+%                     grad(idx) = -2*real(sum(tmp1(:)))/numel(tmp1);
+%                 end
+%             end
+%             func = @objFunc;
+%         end
     end
 end
