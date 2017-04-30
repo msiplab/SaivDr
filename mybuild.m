c1ad8547b32229689ae7b609b9987baafbfaa443
shodimaggio <shogo@eng.niigata-u.ac.jp>
2017-04-30 16:26:22 +0900

    Minor revisionRevised to support MEX code generation independent of the number of channels. Fix to eliminate automatic code generation and generate code with script "mybuild."

---
+++ mybuild.m
@@ -0,0 +1,22 @@
+%MYBUILD Script for building mex codes
+%
+% Requirements: MATLAB R2017a
+%
+% Copyright (c) 2017, Shogo MURAMATSU
+%
+% All rights reserved.
+%
+% Contact address: Shogo MURAMATSU,
+%    Faculty of Engineering, Niigata University,
+%    8050 2-no-cho Ikarashi, Nishi-ku,
+%    Niigata, 950-2181, JAPAN
+%
+% http://msiplab.eng.niigata-u.ac.jp/
+%
+if license('checkout','matlab_coder')
+    %
+    import saivdr.dictionary.olpprfb.mexsrcs.*
+    fcn_build_atomext1d;
+    fcn_build_atomcnc1d;
+    %
+end
