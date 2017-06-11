3c62f1b6f655f65b494c46ad7fd8735d16242f2a
shodimaggio <shogo@eng.niigata-u.ac.jp>
2017-06-11 20:16:12 +0900

    Revised comments.

--- +saivdr/+degradation/DegradationSystem.m
+++ +saivdr/+degradation/DegradationSystem.m
@@ -1,12 +1,9 @@
 classdef DegradationSystem < matlab.System %#codegen
     %DEGRADATIONSYSTEM Degradation system
     %   
-    % SVN identifier:
-    % $Id: DegradationSystem.m 683 2015-05-29 08:22:13Z sho $
-    %
     % Requirements: MATLAB R2015b
     %
-    % Copyright (c) 2014-2015, Shogo MURAMATSU
+    % Copyright (c) 2014-2017, Shogo MURAMATSU
     %
     % All rights reserved.
     %
