d697068e6763ab76c3e63ea3c079b86ecacf4ad7
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2020-08-15 20:25:33 +0900

    Implemented BACKWARD function.

--- +saivdr/+dcnn/nsoltInitialRotation2dLayer.m
+++ +saivdr/+dcnn/nsoltInitialRotation2dLayer.m
@@ -121,6 +121,93 @@ classdef nsoltInitialRotation2dLayer < nnet.layer.Layer
             
         end
         
+        function [Z, memory] = forward(layer, X)
+            % (Optional) Forward input data through the layer at training
+            % time and output the result and a memory value.
+            %
+            % Inputs:
+            %         layer       - Layer to forward propagate through
+            %         X1, ..., Xn - Input data
+            % Outputs:
+            %         Z1, ..., Zm - Outputs of layer forward function
+            %         memory      - Memory value for custom backward propagation
+            
+            % Layer forward function for training goes here.
+            Z = layer.predict(X);
+            memory = X;
+        end
+        
+         function [dLdX, dLdW] = ...
+                backward(layer, ~, ~, dLdZ, memory)
+            % (Optional) Backward propagate the derivative of the loss
+            % function through the layer.
+            %
+            % Inputs:
+            %         layer             - Layer to backward propagate through
+            %         X1, ..., Xn       - Input data
+            %         Z1, ..., Zm       - Outputs of layer forward function
+            %         dLdZ1, ..., dLdZm - Gradients propagated from the next layers
+            %         memory            - Memory value from forward function
+            % Outputs:
+            %         dLdX1, ..., dLdXn - Derivatives of the loss with respect to the
+            %                             inputs
+            %         dLdW1, ..., dLdWk - Derivatives of the loss with respect to each
+            %      
+            import saivdr.dcnn.fcn_orthonormalmatrixgenerate
+            
+            nrows = size(dLdZ,1);
+            ncols = size(dLdZ,2);
+            ps = layer.NumberOfChannels(1);
+            pa = layer.NumberOfChannels(2);
+            nAngles = length(layer.Angles);            
+            nSamples = size(dLdZ,4);
+            stride = layer.DecimationFactor;
+            nDecs = prod(stride);
+            %
+            if isempty(layer.Mus)
+                layer.Mus = ones(ps+pa,1);
+            elseif isscalar(layer.Mus)
+                layer.Mus = layer.Mus*ones(ps+pa,1);
+            end
+            if layer.NoDcLeakage
+                layer.Mus(1) = 1;
+                layer.Angles(1:ps-1) = ...
+                    zeros(ps-1,1,'like',layer.Angles);
+            end            
+            muW = layer.Mus(1:ps);
+            muU = layer.Mus(ps+1:end);
+            anglesW = layer.Angles(1:length(layer.Angles)/2);
+            anglesU = layer.Angles(length(layer.Angles)/2+1:end);
+            W0T = transpose(fcn_orthonormalmatrixgenerate(anglesW,muW,0));
+            U0T = transpose(fcn_orthonormalmatrixgenerate(anglesU,muU,0));
+            
+            % Layer backward function goes here.
+            % dLdX = dZdX x dLdZ
+            Y = permute(dLdZ,[3 1 2 4]);
+            Ys = reshape(Y(1:ps,:,:,:),ps,nrows*ncols*nSamples);
+            Ya = reshape(Y(ps+1:ps+pa,:,:,:),pa,nrows*ncols*nSamples);
+            Zsa = [ W0T(1:nDecs/2,:)*Ys; U0T(1:nDecs/2,:)*Ya ];
+            dLdX = ipermute(reshape(Zsa,nDecs,nrows,ncols,nSamples),...
+                [3 1 2 4]);
+            
+            % dLdWi = <dLdZ,(dVdWi)X>
+            dLdW = zeros(nAngles,1,'like',dLdZ);
+            dldz_ = permute(dLdZ,[3 1 2 4]);
+            dldz_upp = reshape(dldz_(1:ps,:,:,:),ps,nrows*ncols*nSamples);
+            dldz_low = reshape(dldz_(ps+1:ps+pa,:,:,:),pa,nrows*ncols*nSamples);
+            % (dVdWi)X
+            for iAngle = 1:nAngles/2
+                dW0 = fcn_orthonormalmatrixgenerate(anglesW,muW,iAngle);
+                dU0 = fcn_orthonormalmatrixgenerate(anglesU,muU,iAngle);
+                a_ = permute(memory,[3 1 2 4]);
+                c_upp = reshape(a_(1:nDecs/2,:,:,:),nDecs/2,nrows*ncols*nSamples);
+                c_low = reshape(a_(nDecs/2+1:nDecs,:,:,:),nDecs/2,nrows*ncols*nSamples);
+                d_upp = dW0(:,1:nDecs/2)*c_upp;
+                d_low = dU0(:,1:nDecs/2)*c_low;
+                dLdW(iAngle) = sum(dldz_upp.*d_upp,'all');
+                dLdW(nAngles/2+iAngle) = sum(dldz_low.*d_low,'all');
+            end
+         end
     end
     
 end
