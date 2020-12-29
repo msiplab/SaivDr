fd9e95c0168adc31badb6b4aea5a3a33dcc57540
shodimaggio <shogo@eng.niigata-u.ac.jp>
2020-12-29 15:09:14 +0900

    Added test methods

--- appendix/pytorch/nsoltAtomExtension2dLayer.py
+++ appendix/pytorch/nsoltAtomExtension2dLayer.py
@@ -2,7 +2,27 @@ import torch
 import torch.nn as nn
 
 class NsoltAtomExtension2dLayer(nn.Module):
-
+    """
+    %NSOLTATOMEXTENSION2DLAYER
+    %   コンポーネント別に入力(nComponents=1のみサポート):
+    %      nChsTotal x nRows x nCols x nSamples
+    %
+    %   コンポーネント別に出力(nComponents=1のみサポート):
+    %      nChsTotal x nRows x nCols x nSamples
+    %
+    % Requirements: Python 3.x, PyTorch 1.7.x
+    %
+    % Copyright (c) 2020, Shogo MURAMATSU
+    %
+    % All rights reserved.
+    %
+    % Contact address: Shogo MURAMATSU,
+    %                Faculty of Engineering, Niigata University,
+    %                8050 2-no-cho Ikarashi, Nishi-ku,
+    %                Niigata, 950-2181, JAPAN
+    %
+    % http://msiplab.eng.niigata-u.ac.jp/
+    """
     def __init__(self,
             name='',
             number_of_channels=[],
@@ -24,5 +44,47 @@ class NsoltAtomExtension2dLayer(nn.Module):
         self.type = ''        
 
     def forward(self,x):
-        return x
+        dir = self.direction
+        #
+        if dir=='Right':
+            shift = ( 0, 0, 1, 0 )
+        elif dir=='Left':
+            shift = ( 0, 0, -1, 0 )
+        elif dir=='Down':
+            shift = ( 0, 1, 0, 0 )
+        elif dir=='Up':
+            shift = ( 0, -1, 0, 0 )
+        #else
+        #    throw(MException('NsoltLayer:InvalidDirection',...
+        #        '%s : Direction should be either of Right, Left, Down or Up',...
+        #        layer.Direction))
 
+        #
+        return self.atomext_(x,shift)
+
+    def atomext_(self,X,shift):
+        ps = self.number_of_channels[0]
+        pa = self.number_of_channels[1]
+        target = self.target_channels
+        #
+        Y = X
+        # Block butterfly
+        Ys = Y[:ps,:,:,:]
+        Ya = Y[ps:,:,:,:]
+        Y = torch.cat((Ys+Ya,Ys-Ya),dim=0)
+        # Block circular shift
+        if target=='Lower':
+            Y[ps:,:,:,:] = torch.roll(Y[ps:,:,:,:],shifts=shift,dims=(0,1,2,3))
+        elif target=='Upper':
+            Y[:ps,:,:,:] = torch.roll(Y[:ps,:,:,:],shifts=shift,dims=(0,1,2,3))
+        #else:
+        #    throw(MException('NsoltLayer:InvalidTargetChannels',...
+        #        '%s : TaregetChannels should be either of Lower or Upper',...
+        #        layer.TargetChannels))
+
+        # Block butterfly
+        Ys = Y[:ps,:,:,:]
+        Ya = Y[ps:,:,:,:]
+        Y = torch.cat((Ys+Ya, Ys-Ya),dim=0)
+        # Output
+        return Y/2.0
--- appendix/pytorch/test_nsoltAtomExtension2dLayer.py
+++ appendix/pytorch/test_nsoltAtomExtension2dLayer.py
@@ -3,17 +3,18 @@ import unittest
 from parameterized import parameterized
 import torch
 import torch.nn as nn
+import numpy as np 
 from nsoltAtomExtension2dLayer import NsoltAtomExtension2dLayer
 
 nchs = [ [3,3], [4,4] ]
-datatype = [ 'single', 'double' ]
+datatype = [ torch.float, torch.double ]
 nrows = [ 4, 8, 16 ]
 ncols = [ 4, 8, 16 ]
 dir = [ 'Right', 'Left', 'Up', 'Down' ]
 
-class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
+class NsoltAtomExtention2dLayerself(unittest.TestCase):
     """
-    NSOLTATOMEXTENSION2DLAYERTESTCASE
+    NSOLTATOMEXTENSION2DLAYERself
     
         コンポーネント別に入力(nComponents=1のみサポート):
             nChsTotal x nRows x nCols x nSamples
@@ -21,7 +22,7 @@ class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
         コンポーネント別に出力(nComponents=1のみサポート):
             nChsTotal x nRows x nCols x nSamples
     
-    Requirements: Python 3.x
+    Requirements: Python 3.x, PyTorch 1.7.x
 
     Copyright (c) 2020, Shogo MURAMATSU
     
@@ -67,14 +68,63 @@ class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
         self.assertEqual(actualTargetChannels,expctdTargetChannels)
         self.assertEqual(actualDescription,expctdDescription)
 
-    """
-    def testForward(self):
-        target = NsoltAtomExtension2dLayer()
-        x = 0.0
-        valueExpctd = 0.0
-        valueActual = target.forward(x)
-        self.assertEqual(valueExpctd, valueActual)
-    """
+
+    @parameterized.expand(
+        list(itertools.product(nchs,nrows,ncols,dir,datatype))
+    )
+    def testPredictGrayscaleShiftLowerCoefs(self, \
+            nchs, nrows, ncols, dir, datatype):
+        atol= 1e-6
+            
+        # Parameters
+        nSamples = 8
+        nChsTotal = sum(nchs)
+        target = 'Lower'
+        # nChsTotal x nRows x nCols x nSamples
+        X = torch.randn(nChsTotal,nrows,ncols,nSamples,dtype=datatype)
+        
+        # Expected values
+        if dir=='Right':
+            shift = ( 0, 0, 1, 0 )
+        elif dir=='Left':
+            shift = ( 0, 0, -1, 0 )
+        elif dir=='Down':
+            shift = ( 0, 1, 0, 0 )
+        elif dir=='Up':
+            shift = ( 0, -1, 0, 0 )
+        else:
+            shift = ( 0, 0, 0, 0 )
+    
+        # nRows x nCols x nChsTotal x nSamples
+        ps = nchs[0]
+        pa = nchs[1]
+        Y = X 
+        # Block butterfly
+        Ys = Y[:ps,:,:,:]
+        Ya = Y[ps:,:,:,:]
+        Y =  torch.cat((Ys+Ya, Ys-Ya),dim=0)/np.sqrt(2)
+        # Block circular shift
+        Y[ps:,:,:,:] = torch.roll(Y[ps:,:,:,:],shifts=shift,dims=(0,1,2,3))
+        # Block butterfly
+        Ys = Y[:ps,:,:,:]
+        Ya = Y[ps:,:,:,:]
+        Y =  torch.cat((Ys+Ya ,Ys-Ya),dim=0)/np.sqrt(2)
+        # Output
+        expctdZ = Y 
+
+        # Instantiation of target class
+        layer = NsoltAtomExtension2dLayer( \
+            number_of_channels=nchs, \
+            name='Qn~', \
+            direction=dir, \
+            target_channels=target)
+
+        # Actual values
+        actualZ = layer.forward(X)
+        
+        # Evaluation
+        self.assertEqual(actualZ.dtype,datatype) 
+        self.assertTrue(torch.isclose(actualZ,expctdZ,rtol=0.,atol=atol).all())
     
 if __name__ == '__main__':
     unittest.main()
