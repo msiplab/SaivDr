c9310ceed468943a14bdafd62fd822ea6329fa58
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-01-03 16:07:20 +0900

    Added test cases for the forward method

--- appendix/pytorch/nsoltBlockIdct2dLayer.py
+++ appendix/pytorch/nsoltBlockIdct2dLayer.py
@@ -1,10 +1,75 @@
 import torch
 import torch.nn as nn
+import torch_dct as dct
+import numpy as np 
+from nsoltUtility import Direction
 
 class NsoltBlockIdct2dLayer(nn.Module):
-
-    def __init__(self):
+    """
+    NSOLTBLOCKIDCT2DLAYER
+    
+       コンポーネント別に入力(nComponents):
+          nSamples x nRows x nCols x nDecs
+    
+       ベクトル配列をブロック配列にして出力:
+          nSamples x nComponents x (Stride(1)xnRows) x (Stride(2)xnCols) 
+        
+    Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+    Copyright (c) 2020-2021, Shogo MURAMATSU
+    
+    All rights reserved.
+    
+    Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+        http://msiplab.eng.niigata-u.ac.jp/
+    """
+    def __init__(self,
+        name='',
+        decimation_factor=[],
+        number_of_components=1
+        ):
         super(NsoltBlockIdct2dLayer, self).__init__()
+        self.decimation_factor = decimation_factor 
+        self.name = name 
+        self.description = "Block IDCT of size " \
+            + str(self.decimation_factor[Direction.VERTICAL]) + "x" \
+            + str(self.decimation_factor[Direction.HORIZONTAL])
+        #self.type = ''
+        self.num_inputs = number_of_components
 
     def forward(self,x):
-        return x
+        block_size = self.decimation_factor
+        nsamples = x.size(0)
+        nrows = x.size(1)
+        ncols = x.size(2)
+        # Permute IDCT coefficients
+        coefs = x.view(-1,np.prod(block_size))
+        decY_ = block_size[Direction.VERTICAL]
+        decX_ = block_size[Direction.HORIZONTAL]
+        chDecY = np.ceil(decY_/2.).astype(int)
+        chDecX = np.ceil(decX_/2.).astype(int)
+        fhDecY = np.floor(decY_/2.).astype(int)
+        fhDecX = np.floor(decX_/2.).astype(int)
+        nQDecsee = chDecY*chDecX
+        nQDecsoo = fhDecY*fhDecX
+        nQDecsoe = fhDecY*chDecX
+        cee = coefs[:,:nQDecsee]
+        coo = coefs[:,nQDecsee:nQDecsee+nQDecsoo]
+        coe = coefs[:,nQDecsee+nQDecsoo:nQDecsee+nQDecsoo+nQDecsoe]
+        ceo = coefs[:,nQDecsee+nQDecsoo+nQDecsoe:]
+        nBlocks = coefs.size(0)
+        value = torch.zeros(nBlocks,decY_,decX_,dtype=x.dtype)
+        value[:,0::2,0::2] = cee.view(nBlocks,chDecY,chDecX)
+        value[:,1::2,1::2] = coo.view(nBlocks,fhDecY,fhDecX)
+        value[:,1::2,0::2] = coe.view(nBlocks,fhDecY,chDecX)
+        value[:,0::2,1::2] = ceo.view(nBlocks,chDecY,fhDecX)
+        # 2D IDCT
+        y = dct.idct_2d(value,norm='ortho')
+        # Reshape and return
+        height = nrows * decY_ 
+        width = ncols * decX_
+        return y.reshape(nsamples,1,height,width)
--- appendix/pytorch/test_nsoltBlockIdct2dLayer.py
+++ appendix/pytorch/test_nsoltBlockIdct2dLayer.py
@@ -1,20 +1,165 @@
+import itertools
 import unittest
+from parameterized import parameterized
 import torch
 import torch.nn as nn
+import torch_dct as dct
+import numpy as np 
 from nsoltBlockIdct2dLayer import NsoltBlockIdct2dLayer
+from nsoltUtility import Direction
+
+stride = [ [1, 1], [2, 2], [2, 4], [4, 1], [4, 4] ]
+datatype = [ torch.float, torch.double ]
+height = [ 8, 16, 32 ]
+width = [ 8, 16, 32 ]
 
 class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
+    """
+    NSOLTBLOCKIDCT2DLAYERTESTCASE  
+    
+       コンポーネント別に入力:
+          nSamples x nRows x nCols x nDecs 
+    
+       ベクトル配列をブロック配列にして出力:
+          nSamples x nComponents x (Stride(1)xnRows) x (Stride(2)xnCols) 
+    
+    Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+    Copyright (c) 2020-2021, Shogo MURAMATSU
+    
+    All rights reserved.
+    
+    Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+        http://msiplab.eng.niigata-u.ac.jp/
+    """
+    @parameterized.expand(
+        list(itertools.product(stride))
+    )
+    def testConstructor(self,stride):
+        # Expected values
+        expctdName = 'E0~'
+        expctdDescription = "Block IDCT of size " \
+            + str(stride[Direction.VERTICAL])+ "x" \
+            + str(stride[Direction.HORIZONTAL])
+            
+        # Instantiation of target class
+        layer = NsoltBlockIdct2dLayer(
+            decimation_factor=stride,
+            name=expctdName)
+            
+        # Actual values
+        actualName = layer.name
+        actualDescription = layer.description
+            
+        # Evaluation
+        self.assertTrue(isinstance(layer, nn.Module))
+        self.assertEqual(actualName,expctdName)
+        self.assertEqual(actualDescription,expctdDescription)
 
-    def testConstructor(self):
-        target = NsoltBlockIdct2dLayer()
-        self.assertTrue(isinstance(target, nn.Module))
+    @parameterized.expand(
+        list(itertools.product(stride,height,width,datatype))
+    )
+    def testPredictGrayScale(self,
+        stride, height, width, datatype):
+        atol = 1e-6
 
-    def testForward(self):
-        target = NsoltBlockIdct2dLayer()
-        x = 0.0
-        valueExpctd = 0.0
-        valueActual = target.forward(x)
-        self.assertEqual(valueExpctd, valueActual)
+        # Parameters
+        nSamples = 8
+        nrows = np.ceil(height/stride[Direction.VERTICAL]).astype(int)
+        ncols = np.ceil(width/stride[Direction.HORIZONTAL]).astype(int)
+        nDecs = np.prod(stride)
+        nComponents = 1
+        # nSamples x nRows x nCols x nDecs         
+        X = torch.rand(nSamples,nrows,ncols,nDecs,dtype=datatype,requires_grad=True)
+
+        # Expected values
+        A = permuteIdctCoefs_(X,stride)
+        Y = dct.idct_2d(A,norm='ortho')
+        expctdZ = Y.reshape(nSamples,nComponents,height,width)
+
+        # Instantiation of target class
+        layer = NsoltBlockIdct2dLayer(
+               decimation_factor=stride,
+                name='E0~'
+            )
+
+        # Actual values
+        with torch.no_grad():
+            actualZ = layer.forward(X)
+
+        # Evaluation
+        self.assertEqual(actualZ.dtype,datatype)
+        self.assertTrue(torch.isclose(actualZ,expctdZ,rtol=0.,atol=atol).all())
+        self.assertFalse(actualZ.requires_grad)
+
+    @parameterized.expand(
+        list(itertools.product(stride,height,width,datatype))
+    )
+    def testForwardGrayScale(self,
+        stride, height, width, datatype):
+        atol = 1e-6
+
+        # Parameters
+        nSamples = 8
+        nrows = np.ceil(height/stride[Direction.VERTICAL]).astype(int)
+        ncols = np.ceil(width/stride[Direction.HORIZONTAL]).astype(int)
+        nDecs = np.prod(stride)
+        nComponents = 1
+        # nSamples x nRows x nCols x nDecs         
+        X = torch.rand(nSamples,nrows,ncols,nDecs,dtype=datatype,requires_grad=True)
+
+        # Expected values
+        A = permuteIdctCoefs_(X,stride)
+        Y = dct.idct_2d(A,norm='ortho')
+        expctdZ = Y.reshape(nSamples,nComponents,height,width)
+
+        # Instantiation of target class
+        layer = NsoltBlockIdct2dLayer(
+               decimation_factor=stride,
+                name='E0~'
+            )
+
+        # Actual values
+        actualZ = layer.forward(X)
+
+        # Evaluation
+        self.assertEqual(actualZ.dtype,datatype)
+        self.assertTrue(torch.isclose(actualZ,expctdZ,rtol=0.,atol=atol).all())
+        self.assertTrue(actualZ.requires_grad)
+
+def permuteDctCoefs_(x):
+    cee = x[:,0::2,0::2].reshape(x.size(0),-1)
+    coo = x[:,1::2,1::2].reshape(x.size(0),-1)
+    coe = x[:,1::2,0::2].reshape(x.size(0),-1)
+    ceo = x[:,0::2,1::2].reshape(x.size(0),-1)
+    return torch.cat((cee,coo,coe,ceo),dim=-1)
+
+def permuteIdctCoefs_(x,block_size):
+    coefs = x.view(-1,np.prod(block_size))
+    decY_ = block_size[Direction.VERTICAL]
+    decX_ = block_size[Direction.HORIZONTAL]
+    chDecY = np.ceil(decY_/2.).astype(int)
+    chDecX = np.ceil(decX_/2.).astype(int)
+    fhDecY = np.floor(decY_/2.).astype(int)
+    fhDecX = np.floor(decX_/2.).astype(int)
+    nQDecsee = chDecY*chDecX
+    nQDecsoo = fhDecY*fhDecX
+    nQDecsoe = fhDecY*chDecX
+    cee = coefs[:,:nQDecsee]
+    coo = coefs[:,nQDecsee:nQDecsee+nQDecsoo]
+    coe = coefs[:,nQDecsee+nQDecsoo:nQDecsee+nQDecsoo+nQDecsoe]
+    ceo = coefs[:,nQDecsee+nQDecsoo+nQDecsoe:]
+    nBlocks = coefs.size(0)
+    value = torch.zeros(nBlocks,decY_,decX_,dtype=x.dtype)
+    value[:,0::2,0::2] = cee.view(nBlocks,chDecY,chDecX)
+    value[:,1::2,1::2] = coo.view(nBlocks,fhDecY,fhDecX)
+    value[:,1::2,0::2] = coe.view(nBlocks,fhDecY,chDecX)
+    value[:,0::2,1::2] = ceo.view(nBlocks,chDecY,fhDecX)
+    return value
 
 if __name__ == '__main__':
     unittest.main()
