1bd7c87c872f54f2b4399aad9a9b4fe5089cfdb2
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-01-04 14:30:10 +0900

    Added test cases for the  backward method

--- appendix/pytorch/nsoltChannelConcatenation2dLayer.py
+++ appendix/pytorch/nsoltChannelConcatenation2dLayer.py
@@ -2,9 +2,49 @@ import torch
 import torch.nn as nn
 
 class NsoltChannelConcatenation2dLayer(nn.Module):
+    """
+    NSOLTCHANNELCONCATENATION2DLAYER
+    
+       ２コンポーネント入力(nComponents=2のみサポート):
+          nSamples x nRows x nCols x (nChsTotal-1) 
+          nSamples x nRows x nCols 
+    
+       １コンポーネント出力(nComponents=1のみサポート):
+          nSamples x nRows x nCols x nChsTotal
+    
+     Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+     Copyright (c) 2020-2021, Shogo MURAMATSU
+    
+     All rights reserved.
+    
+     Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+        http://msiplab.eng.niigata-u.ac.jp/
+    """
 
-    def __init__(self):
+    def __init__(self,
+        name=''):
         super(NsoltChannelConcatenation2dLayer, self).__init__()
+        self.name = name
+        self.description = "Channel concatenation"
+        #self.type = ''
+        #self.input_names = [ 'ac', 'dc' ]
 
-    def forward(self,x):
-        return x
+    def forward(self,Xac,Xdc):
+        """
+        Forward input data through the layer at prediction time and
+        output the result.
+            
+            Inputs:
+                layer       - Layer to forward propagate through
+                X1, X2      - Input data (2 components)
+            Outputs:
+                Z           - Outputs of layer forward function
+        """
+            
+        # Layer forward function for prediction goes here.
+        return torch.cat((Xdc.unsqueeze(dim=3),Xac),dim=3)
--- appendix/pytorch/test_nsoltChannelConcatenation2dLayer.py
+++ appendix/pytorch/test_nsoltChannelConcatenation2dLayer.py
@@ -1,20 +1,137 @@
+import itertools
 import unittest
+from parameterized import parameterized
 import torch
 import torch.nn as nn
+import numpy as np
 from nsoltChannelConcatenation2dLayer import NsoltChannelConcatenation2dLayer
 
 class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
+    """
+    NSOLTCHANNELCONCATENATION2DLAYERTESTCASE
+    
+       ２コンポーネント入力(nComponents=2のみサポート):
+          nSamples x nRows x nCols x (nChsTotal-1) 
+          nSamples x nRows x nCols
+    
+       １コンポーネント出力(nComponents=1のみサポート):
+          nSamples x nRows x nCols x nChsTotal
+    
+     Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+     Copyright (c) 2020-2021, Shogo MURAMATSU
+    
+     All rights reserved.
+    
+     Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+        http://msiplab.eng.niigata-u.ac.jp/
+    """
+
+    nchs = [ [3, 3], [4, 4] ]
+    datatype = [ torch.float, torch.double ]
+    nrows = [ 4, 8, 16 ]
+    ncols = [ 4, 8, 16 ]
 
     def testConstructor(self):
-        target = NsoltChannelConcatenation2dLayer()
-        self.assertTrue(isinstance(target, nn.Module))
+        #target = NsoltChannelConcatenation2dLayer()
+        #self.assertTrue(isinstance(target, nn.Module))
+        
+        # Expected values
+        expctdName = 'Cn'
+        expctdDescription = "Channel concatenation"
+            
+        # Instantiation of target class
+        layer = NsoltChannelConcatenation2dLayer(
+                name=expctdName
+            )
+            
+        # Actual values
+        actualName = layer.name
+        actualDescription = layer.description
+            
+        # Evaluation
+        self.assertTrue(isinstance(layer, nn.Module))                
+        self.assertEqual(actualName,expctdName)    
+        self.assertEqual(actualDescription,expctdDescription)        
 
-    def testForward(self):
-        target = NsoltChannelConcatenation2dLayer()
-        x = 0.0
-        valueExpctd = 0.0
-        valueActual = target.forward(x)
-        self.assertEqual(valueExpctd, valueActual)
+    @parameterized.expand(
+        list(itertools.product(nchs,nrows,ncols,datatype))
+    )
+    def testPredict(self,
+        nchs,nrows,ncols,datatype):
+        atol=1e-6
+
+        # Parameters
+        nSamples = 8
+        nChsTotal = sum(nchs)
+
+        # nSamples x nRows x nCols x (nChsTotal-1)
+        Xac = torch.randn(nSamples,nrows,ncols,nChsTotal-1,dtype=datatype,requires_grad=True)
+        # nSamples x nRows x nCols 
+        Xdc = torch.randn(nSamples,nrows,ncols,dtype=datatype,requires_grad=True)
+
+        # Expected values
+        # nSamples x nRows x nCols x nChsTotal
+        expctdZ = torch.cat((Xdc.unsqueeze(dim=3),Xac),dim=3)
+
+        # Instantiation of target class
+        layer = NsoltChannelConcatenation2dLayer(
+                name='Cn'
+            )
+
+        # Actual values
+        with torch.no_grad():
+            actualZ = layer.forward(Xac=Xac,Xdc=Xdc)
+
+        # Evaluation
+        self.assertEqual(actualZ.dtype,datatype)
+        self.assertTrue(torch.isclose(actualZ,expctdZ,rtol=0.,atol=atol).all())
+        self.assertFalse(actualZ.requires_grad)
+
+    @parameterized.expand(
+        list(itertools.product(nchs,nrows,ncols,datatype))
+    )
+    def testBackward(self,
+        nchs,nrows,ncols,datatype):
+        atol=1e-6
+    
+        # Parameters
+        nSamples = 8
+        nChsTotal = sum(nchs)
+        # nSamples x nRows x nCols x (nChsTotal-1)
+        Xac = torch.randn(nSamples,nrows,ncols,nChsTotal-1,dtype=datatype,requires_grad=True)
+        # nSamples x nRows x nCols 
+        Xdc = torch.randn(nSamples,nrows,ncols,dtype=datatype,requires_grad=True)
+        # nSamples x nRows x nCols x nChsTotal
+        dLdZ = torch.randn(nSamples,nrows,ncols,nChsTotal,dtype=datatype)
+    
+        # Expected values
+        # nSamples x nRows x nCols x (nChsTotal-1) 
+        expctddLdXac = dLdZ[:,:,:,1:]
+        # nSamples x nRows x nCols x 1  
+        expctddLdXdc = dLdZ[:,:,:,0]
+
+        # Instantiation of target class
+        layer = NsoltChannelConcatenation2dLayer(
+                name='Cn'
+            )
+
+        # Actual values
+        Z = layer.forward(Xac=Xac,Xdc=Xdc)
+        Z.backward(dLdZ)
+        actualdLdXac = Xac.grad
+        actualdLdXdc = Xdc.grad
+
+        # Evaluation
+        self.assertEqual(actualdLdXdc.dtype,datatype)
+        self.assertEqual(actualdLdXac.dtype,datatype)    
+        self.assertTrue(torch.isclose(actualdLdXdc,expctddLdXdc,rtol=0.,atol=atol).all())
+        self.assertTrue(torch.isclose(actualdLdXac,expctddLdXac,rtol=0.,atol=atol).all())        
+        self.assertTrue(Z.requires_grad)
 
 if __name__ == '__main__':
     unittest.main()
