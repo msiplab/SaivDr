bb0b95f18493b84b31219a651bf54b4b0ba2a997
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-01-05 11:04:40 +0900

    Added test cases for the forward method

--- appendix/pytorch/nsoltFinalRotation2dLayer.py
+++ appendix/pytorch/nsoltFinalRotation2dLayer.py
@@ -1,10 +1,94 @@
 import torch
 import torch.nn as nn
+from nsoltUtility import Direction
+import numpy as np
 
 class NsoltFinalRotation2dLayer(nn.Module):
+    """
+    NSOLTFINALROTATION2DLAYER 
+    
+       コンポーネント別に入力(nComponents):
+          nSamples x nRows x nCols x nChs
+    
+       コンポーネント別に出力(nComponents):
+          nSamples x nRows x nCols x nDecs
+    
+    Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+    Copyright (c) 2020-2021, Shogo MURAMATSU
+    
+    All rights reserved.
+    
+    Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+        http://msiplab.eng.niigata-u.ac.jp/ 
+    """
 
-    def __init__(self):
+    def __init__(self,
+        number_of_channels=[],
+        decimation_factor=[],
+        name=''):
         super(NsoltFinalRotation2dLayer, self).__init__()
+        self.name = name
+        self.number_of_channels = number_of_channels
+        self.decimation_factor = decimation_factor
+        self.description = "NSOLT final rotation " \
+                + "(ps,pa) = (" \
+                + str(self.number_of_channels[0]) + "," \
+                + str(self.number_of_channels[1]) + "), "  \
+                + "(mv,mh) = (" \
+                + str(self.decimation_factor[Direction.VERTICAL]) + "," \
+                + str(self.decimation_factor[Direction.HORIZONTAL]) + ")"
 
-    def forward(self,x):
-        return x
+    def forward(self,X):
+        nSamples = X.size(dim=0)
+        nrows = X.size(dim=1)
+        ncols = X.size(dim=2)
+        ps, pa = self.number_of_channels
+        stride = self.decimation_factor
+        nDecs = np.prod(stride)
+
+        W0T = torch.eye(ps,dtype=X.dtype)
+        U0T = torch.eye(pa,dtype=X.dtype)
+
+        Y = X
+        Ys = Y[:,:,:,:ps].view(-1,ps).T
+        Ya = Y[:,:,:,ps:].view(-1,pa).T 
+        Zsa = torch.cat(        
+            ( W0T[:np.ceil(nDecs/2.).astype(int),:].mm(Ys),
+             U0T[:np.floor(nDecs/2.).astype(int),:].mm(Ya) ),
+             dim=0 )
+        return Zsa.T.view(nSamples,nrows,ncols,nDecs)
+        """
+            import saivdr.dcnn.fcn_orthmtxgen
+            
+            % Layer forward function for prediction goes here.
+            if isempty(layer.Mus)
+                layer.Mus = ones(ps+pa,1);
+            elseif isscalar(layer.Mus)
+                layer.Mus = layer.Mus*ones(ps+pa,1);
+            end
+            if layer.NoDcLeakage
+                layer.Mus(1) = 1;
+                layer.Angles(1:ps-1) = ...
+                    zeros(ps-1,1,'like',layer.Angles);
+            end            
+            muW = layer.Mus(1:ps);
+            muU = layer.Mus(ps+1:end);
+            anglesW = layer.Angles(1:length(layer.Angles)/2);
+            anglesU = layer.Angles(length(layer.Angles)/2+1:end);
+            W0T = transpose(fcn_orthmtxgen(anglesW,muW));
+            U0T = transpose(fcn_orthmtxgen(anglesU,muU));
+
+            Y = X; %permute(X,[3 1 2 4]);
+            Ys = reshape(Y(1:ps,:,:,:),ps,nrows*ncols*nSamples);
+            Ya = reshape(Y(ps+1:ps+pa,:,:,:),pa,nrows*ncols*nSamples);
+            Zsa = [ W0T(1:ceil(nDecs/2),:)*Ys; U0T(1:floor(nDecs/2),:)*Ya ];
+            %Z = ipermute(reshape(Zsa,nDecs,nrows,ncols,nSamples),...
+            %    [3 1 2 4]);
+            Z = reshape(Zsa,nDecs,nrows,ncols,nSamples);
+        """
+        return X
--- appendix/pytorch/test_nsoltFinalRotation2dLayer.py
+++ appendix/pytorch/test_nsoltFinalRotation2dLayer.py
@@ -1,20 +1,114 @@
+import itertools
 import unittest
+from parameterized import parameterized
 import torch
 import torch.nn as nn
+import numpy as np
 from nsoltFinalRotation2dLayer import NsoltFinalRotation2dLayer
+from nsoltUtility import Direction
 
-class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
+nchs = [ [2, 2], [3, 3], [4, 4] ]
+stride = [ [1, 1], [1, 2], [2, 2] ]
+mus = [ -1, 1 ]
+datatype = [ torch.float, torch.double ]
+nrows = [ 4, 8, 16 ]
+ncols = [ 4, 8, 16 ]
 
-    def testConstructor(self):
-        target = NsoltFinalRotation2dLayer()
-        self.assertTrue(isinstance(target, nn.Module))
+class NsoltFinalRotation2dLayerTestCase(unittest.TestCase):
+    """
+    NSOLTFINALROTATION2DLAYERTESTCASE 
+    
+       コンポーネント別に入力(nComponents):
+          nSamples x nRows x nCols x nChs
+    
+       コンポーネント別に出力(nComponents):
+          nSamples x nRows x nCols x nDecs
+    
+    Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+    Copyright (c) 2020-2021, Shogo MURAMATSU
+    
+    All rights reserved.
+    
+    Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+        http://msiplab.eng.niigata-u.ac.jp/ 
+    """
 
-    def testForward(self):
-        target = NsoltFinalRotation2dLayer()
-        x = 0.0
-        valueExpctd = 0.0
-        valueActual = target.forward(x)
-        self.assertEqual(valueExpctd, valueActual)
+    @parameterized.expand(
+        list(itertools.product(nchs,stride))
+    )
+    def testConstructor(self,
+        nchs, stride):
+        
+        # Expected values
+        expctdName = 'V0~'
+        expctdDescription = "NSOLT final rotation " \
+                + "(ps,pa) = (" \
+                + str(nchs[0]) + "," + str(nchs[1]) + "), " \
+                + "(mv,mh) = (" \
+                + str(stride[Direction.VERTICAL]) + "," + str(stride[Direction.HORIZONTAL]) + ")"
+
+        # Instantiation of target class
+        layer = NsoltFinalRotation2dLayer(
+                number_of_channels=nchs,
+                decimation_factor=stride,
+                name=expctdName
+            )            
+
+        # Actual values
+        actualName = layer.name
+        actualDescription = layer.description
+
+        # Evaluation
+        self.assertTrue(isinstance(layer, nn.Module))
+        self.assertEqual(actualName,expctdName)
+        self.assertEqual(actualDescription,expctdDescription)
+
+    @parameterized.expand(
+        list(itertools.product(nchs,stride,nrows,ncols,datatype))
+    )
+    def testPredictGrayscale(self,
+        nchs, stride, nrows, ncols, datatype):
+        atol=1e-6
+
+        # Parameters
+        nSamples = 8
+        nDecs = np.prod(stride)
+        # nSamples x nRows x nCols x nChs
+        X = torch.randn(nSamples,nrows,ncols,sum(nchs),dtype=datatype,requires_grad=True)
+        
+        # Expected values        
+        # nSamples x nRows x nCols x nDecs
+        ps, pa = nchs
+        W0T = torch.eye(ps,dtype=datatype)
+        U0T = torch.eye(pa,dtype=datatype)
+        Y = X
+        Ys = Y[:,:,:,:ps].view(-1,ps).T
+        Ya = Y[:,:,:,ps:].view(-1,pa).T
+        Zsa = torch.cat(
+                ( W0T[:np.ceil(nDecs/2.).astype(int),:].mm(Ys), 
+                  U0T[:np.floor(nDecs/2.).astype(int),:].mm(Ya) ),dim=0)
+        expctdZ = Zsa.T.view(nSamples,nrows,ncols,nDecs)
+
+        # Instantiation of target class
+        layer = NsoltFinalRotation2dLayer(
+                number_of_channels=nchs,
+                decimation_factor=stride,
+                name='V0~'
+            )
+
+        # Actual values
+        with torch.no_grad():
+            actualZ = layer.forward(X)
+
+        # Evaluation
+        self.assertEqual(actualZ.dtype,datatype)
+        self.assertTrue(torch.isclose(actualZ,expctdZ,rtol=0.,atol=atol).all())
+        self.assertFalse(actualZ.requires_grad)
 
 if __name__ == '__main__':
     unittest.main()
