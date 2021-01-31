38b7f09fb7cbaf0926f591858e207c2667997236
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-01-31 12:10:05 +0900

    Added test cases for the backward method

--- appendix/pytorch/test_nsoltInitialRotation2dLayer.py
+++ appendix/pytorch/test_nsoltInitialRotation2dLayer.py
@@ -233,7 +233,6 @@ class NsoltInitialRotation2dLayerTestCase(unittest.TestCase):
         anglesW = torch.zeros(nAnglesH,dtype=datatype)
         anglesU = torch.zeros(nAnglesH,dtype=datatype)
         mus = 1
-
         # nSamples x nRows x nCols x nDecs
         X = torch.randn(nSamples,nrows,ncols,nDecs,dtype=datatype,requires_grad=True)
         dLdZ = torch.randn(nSamples,nrows,ncols,nChsTotal,dtype=datatype)
@@ -294,179 +293,164 @@ class NsoltInitialRotation2dLayerTestCase(unittest.TestCase):
         self.assertTrue(torch.allclose(actualdLdW_U,expctddLdW_U,rtol=rtol,atol=atol))
         self.assertTrue(Z.requires_grad)
 
-    """        
-        function testBackwardGrayscaleWithRandomAngles(testCase, ...
-                nchs, stride, nrows, ncols, datatype)
+
+    @parameterized.expand(
+        list(itertools.product(nchs,stride,nrows,ncols,datatype))
+    )
+    def testBackwardGrayscaleWithRandomAngles(self,
+        nchs, stride, nrows, ncols, datatype):
+        rtol,atol=1e-3,1e-6
+        omgs = OrthonormalMatrixGenerationSystem(dtype=datatype,partial_difference=False)
+
+        # Parameters
+        nSamples = 8
+        nDecs = stride[0]*stride[1] # math.prod(stride)
+        nChsTotal = sum(nchs)
+        nAnglesH = int((nChsTotal-2)*nChsTotal/8)
+        anglesW = torch.randn(nAnglesH,dtype=datatype)
+        anglesU = torch.randn(nAnglesH,dtype=datatype)
+        mus = 1
+        # nSamples x nRows x nCols x nDecs
+        X = torch.randn(nSamples,nrows,ncols,nDecs,dtype=datatype,requires_grad=True)
+        dLdZ = torch.randn(nSamples,nrows,ncols,nChsTotal,dtype=datatype)
+
+        # Expected values
+        ps,pa = nchs
+        W0T = omgs(anglesW,mus).T
+        U0T = omgs(anglesU,mus).T
+        # dLdX = dZdX x dLdZ
+        ms,ma = int(math.ceil(nDecs/2.)),int(math.floor(nDecs/2.))
+        Ys = dLdZ[:,:,:,:ps].view(nSamples*nrows*ncols,ps).T # ps * n
+        Ya = dLdZ[:,:,:,ps:].view(nSamples*nrows*ncols,pa).T # pa * n
+        Y = torch.cat(
+            ( W0T[:ms,:] @ Ys,          # ms x ps @ ps x n
+              U0T[:ma,:] @ Ya ), dim=0) # ma x pa @ pa x n
+        expctddLdX = Y.T.view(nSamples,nrows,ncols,nDecs) # n x (ms+ma)
+        # dLdWi = <dLdZ,(dVdWi)X>
+        expctddLdW_W = torch.zeros(nAnglesH,dtype=datatype)
+        expctddLdW_U = torch.zeros(nAnglesH,dtype=datatype)
+        omgs.partial_difference = True
+        for iAngle in range(nAnglesH):
+            dW0 = omgs(anglesW,mus,index_pd_angle=iAngle)
+            Xs = X[:,:,:,:ms].view(-1,ms).T 
+            Zs = dW0[:,:ms] @ Xs # ps x n
+            expctddLdW_W[iAngle] = torch.sum(Ys * Zs) # ps x n
+            if ma>0:
+                dU0 = omgs(anglesU,mus,index_pd_angle=iAngle)
+                Xa = X[:,:,:,ms:].view(-1,ma).T
+                Za = dU0[:,:ma] @ Xa # pa x n            
+                expctddLdW_U[iAngle] = torch.sum(Ya * Za) # pa x n
             
-            import matlab.unittest.constraints.IsEqualTo
-            import matlab.unittest.constraints.AbsoluteTolerance
-            tolObj = AbsoluteTolerance(1e-4,single(1e-4));
-            import saivdr.dictionary.utility.*
-            genW = OrthonormalMatrixGenerationSystem(...
-                'PartialDifference','on');
-            genU = OrthonormalMatrixGenerationSystem(...
-                'PartialDifference','on');
+        # Instantiation of target class
+        layer = NsoltInitialRotation2dLayer(
+            number_of_channels=nchs,
+            decimation_factor=stride,
+            name='V0')
+        layer.orthTransW0.angles.data = anglesW
+        layer.orthTransW0.mus = mus
+        layer.orthTransU0.angles.data = anglesU
+        layer.orthTransU0.mus = mus
+
+        # Actual values
+        torch.autograd.set_detect_anomaly(True)
+        Z = layer.forward(X)
+        layer.zero_grad()
+        Z.backward(dLdZ)
+        actualdLdX = X.grad
+        actualdLdW_W = layer.orthTransW0.angles.grad
+        actualdLdW_U = layer.orthTransU0.angles.grad
+
+        # Evaluation
+        self.assertEqual(actualdLdX.dtype,datatype)
+        self.assertEqual(actualdLdW_W.dtype,datatype)
+        self.assertEqual(actualdLdW_U.dtype,datatype)
+        self.assertTrue(torch.allclose(actualdLdX,expctddLdX,rtol=rtol,atol=atol))
+        self.assertTrue(torch.allclose(actualdLdW_W,expctddLdW_W,rtol=rtol,atol=atol))
+        self.assertTrue(torch.allclose(actualdLdW_U,expctddLdW_U,rtol=rtol,atol=atol))
+        self.assertTrue(Z.requires_grad)
+
+    @parameterized.expand(
+        list(itertools.product(nchs,stride,nrows,ncols,datatype,mus))
+    )
+    def testBackwardGrayscaleWithRandomAnglesNoDcLeackage(self,
+        nchs, stride, nrows, ncols, datatype,mus):
+        rtol,atol=1e-2,1e-5
+        omgs = OrthonormalMatrixGenerationSystem(dtype=datatype,partial_difference=False)
+
+        # Parameters
+        nSamples = 8
+        nDecs = stride[0]*stride[1] # math.prod(stride)
+        nChsTotal = sum(nchs)
+        nAnglesH = int((nChsTotal-2)*nChsTotal/8)
+        anglesW = torch.randn(nAnglesH,dtype=datatype)
+        anglesU = torch.randn(nAnglesH,dtype=datatype)
+        mus = 1
+        # nSamples x nRows x nCols x nDecs
+        X = torch.randn(nSamples,nrows,ncols,nDecs,dtype=datatype,requires_grad=True)
+        dLdZ = torch.randn(nSamples,nrows,ncols,nChsTotal,dtype=datatype)
+
+        # Expected values
+        ps,pa = nchs
+        anglesWNoDcLeak = anglesW.clone()
+        anglesWNoDcLeak[:ps-1] = torch.zeros(ps-1,dtype=datatype)
+        musW,musU = mus*torch.ones(ps,dtype=datatype),mus*torch.ones(pa,dtype=datatype)
+        musW[0] = 1        
+        W0T = omgs(anglesWNoDcLeak,musW).T
+        U0T = omgs(anglesU,musU).T
+        # dLdX = dZdX x dLdZ
+        ms,ma = int(math.ceil(nDecs/2.)),int(math.floor(nDecs/2.))
+        Ys = dLdZ[:,:,:,:ps].view(nSamples*nrows*ncols,ps).T # ps * n
+        Ya = dLdZ[:,:,:,ps:].view(nSamples*nrows*ncols,pa).T # pa * n
+        Y = torch.cat(
+            ( W0T[:ms,:] @ Ys,          # ms x ps @ ps x n
+              U0T[:ma,:] @ Ya ), dim=0) # ma x pa @ pa x n
+        expctddLdX = Y.T.view(nSamples,nrows,ncols,nDecs) # n x (ms+ma)
+        # dLdWi = <dLdZ,(dVdWi)X>
+        expctddLdW_W = torch.zeros(nAnglesH,dtype=datatype)
+        expctddLdW_U = torch.zeros(nAnglesH,dtype=datatype)
+        omgs.partial_difference = True
+        for iAngle in range(nAnglesH):
+            dW0 = omgs(anglesWNoDcLeak,mus,index_pd_angle=iAngle)
+            Xs = X[:,:,:,:ms].view(-1,ms).T 
+            Zs = dW0[:,:ms] @ Xs # ps x n
+            expctddLdW_W[iAngle] = torch.sum(Ys * Zs) # ps x n
+            if ma>0:
+                dU0 = omgs(anglesU,mus,index_pd_angle=iAngle)
+                Xa = X[:,:,:,ms:].view(-1,ma).T
+                Za = dU0[:,:ma] @ Xa # pa x n            
+                expctddLdW_U[iAngle] = torch.sum(Ya * Za) # pa x n
             
-            % Parameters
-            nSamples = 8;
-            nDecs = prod(stride);
-            nChsTotal = sum(nchs);
-            nAnglesH = (nChsTotal-2)*nChsTotal/8;
-            anglesW = randn(nAnglesH,1,datatype);
-            anglesU = randn(nAnglesH,1,datatype);
-            mus_ = 1;
-            
-            % nDecs x nRows x nCols x nSamples
-            %X = randn(nrows,ncols,nDecs,nSamples,datatype);
-            %dLdZ = randn(nrows,ncols,sum(nchs),nSamples,datatype);
-            X = randn(nDecs,nrows,ncols,nSamples,datatype);
-            dLdZ = randn(sum(nchs),nrows,ncols,nSamples,datatype);
-            
-            % Expected values
-            % nDecs x nRows x nCols x nSamples
-            ps = nchs(1);
-            pa = nchs(2);
-            
-            % dLdX = dZdX x dLdZ
-            W0T = transpose(genW.step(anglesW,mus_,0));
-            U0T = transpose(genU.step(anglesU,mus_,0));
-            Y = dLdZ; %permute(dLdZ,[3 1 2 4]);
-            Ys = reshape(Y(1:ps,:,:,:),ps,nrows*ncols*nSamples);
-            Ya = reshape(Y(ps+1:ps+pa,:,:,:),pa,nrows*ncols*nSamples);
-            Zsa = [ W0T(1:ceil(nDecs/2),:)*Ys; U0T(1:floor(nDecs/2),:)*Ya ];
-            %expctddLdX = ipermute(reshape(Zsa,nDecs,nrows,ncols,nSamples),...
-            %    [3 1 2 4]);
-            expctddLdX = reshape(Zsa,nDecs,nrows,ncols,nSamples);
-                        
-            % dLdWi = <dLdZ,(dVdWi)X>
-            expctddLdW = zeros(2*nAnglesH,1,datatype);
-            dldz_ = dLdZ; %permute(dLdZ,[3 1 2 4]);
-            dldz_upp = reshape(dldz_(1:ps,:,:,:),ps,nrows*ncols*nSamples);
-            dldz_low = reshape(dldz_(ps+1:ps+pa,:,:,:),pa,nrows*ncols*nSamples);
-            % (dVdWi)X
-            for iAngle = 1:nAnglesH
-                dW0 = genW.step(anglesW,mus_,iAngle);
-                dU0 = genU.step(anglesU,mus_,iAngle);
-                a_ = X; %permute(X,[3 1 2 4]);
-                c_upp = reshape(a_(1:ceil(nDecs/2),:,:,:),ceil(nDecs/2),nrows*ncols*nSamples);
-                c_low = reshape(a_(ceil(nDecs/2)+1:nDecs,:,:,:),floor(nDecs/2),nrows*ncols*nSamples);
-                d_upp = dW0(:,1:ceil(nDecs/2))*c_upp;
-                d_low = dU0(:,1:floor(nDecs/2))*c_low;
-                expctddLdW(iAngle) = sum(dldz_upp.*d_upp,'all');
-                expctddLdW(nAnglesH+iAngle) = sum(dldz_low.*d_low,'all');
-            end
-            
-            % Instantiation of target class
-            import saivdr.dcnn.*
-            layer = nsoltInitialRotation2dLayer(...
-                'NumberOfChannels',nchs,...
-                'DecimationFactor',stride,...
-                'Name','V0');
-            layer.Mus = mus_;
-            layer.Angles = [anglesW; anglesU];
-            %expctdZ = layer.predict(X);
-            
-            % Actual values
-            [actualdLdX,actualdLdW] = layer.backward(X,[],dLdZ,[]);
-            
-            % Evaluation
-            testCase.verifyInstanceOf(actualdLdX,datatype);
-            testCase.verifyInstanceOf(actualdLdW,datatype);
-            testCase.verifyThat(actualdLdX,...
-                IsEqualTo(expctddLdX,'Within',tolObj));
-            testCase.verifyThat(actualdLdW,...
-                IsEqualTo(expctddLdW,'Within',tolObj));
-            
-        end
-        
-        function testBackwardGrayscaleWithRandomAnglesNoDcLeackage(testCase, ...
-                nchs, stride, nrows, ncols, mus, datatype)
-            
-            import matlab.unittest.constraints.IsEqualTo
-            import matlab.unittest.constraints.AbsoluteTolerance
-            tolObj = AbsoluteTolerance(1e-4,single(1e-4));
-            import saivdr.dictionary.utility.*
-            genW = OrthonormalMatrixGenerationSystem(...
-                'PartialDifference','on');
-            genU = OrthonormalMatrixGenerationSystem(...
-                'PartialDifference','on');
-            
-            % Parameters
-            nSamples = 8;
-            nDecs = prod(stride);
-            nChsTotal = sum(nchs);
-            nAnglesH = (nChsTotal-2)*nChsTotal/8;
-            anglesW = randn(nAnglesH,1,datatype);
-            anglesU = randn(nAnglesH,1,datatype);
-            
-            % nDecs x nRows x nCols x nSamples
-            %X = randn(nrows,ncols,nDecs,nSamples,datatype);
-            %dLdZ = randn(nrows,ncols,sum(nchs),nSamples,datatype);
-            X = randn(nDecs,nrows,ncols,nSamples,datatype);
-            dLdZ = randn(sum(nchs),nrows,ncols,nSamples,datatype);            
-            
-            % Expected values
-            % nDecs x nRows x nCols x nSamples
-            ps = nchs(1);
-            pa = nchs(2);
-            
-            % dLdX = dZdX x dLdZ
-            anglesW_NoDc = anglesW;
-            anglesW_NoDc(1:ps-1,1)=zeros(ps-1,1);
-            musW = mus*ones(ps,1);
-            musW(1,1) = 1;
-            musU = mus*ones(pa,1);            
-            W0T = transpose(genW.step(anglesW_NoDc,musW,0));
-            U0T = transpose(genU.step(anglesU,musU,0));
-            Y = dLdZ; %permute(dLdZ,[3 1 2 4]);
-            Ys = reshape(Y(1:ps,:,:,:),ps,nrows*ncols*nSamples);
-            Ya = reshape(Y(ps+1:ps+pa,:,:,:),pa,nrows*ncols*nSamples);
-            Zsa = [ W0T(1:ceil(nDecs/2),:)*Ys; U0T(1:floor(nDecs/2),:)*Ya ];
-            %expctddLdX = ipermute(reshape(Zsa,nDecs,nrows,ncols,nSamples),...
-            %    [3 1 2 4]);
-            expctddLdX = reshape(Zsa,nDecs,nrows,ncols,nSamples);
-            
-            % dLdWi = <dLdZ,(dVdWi)X>
-            expctddLdW = zeros(2*nAnglesH,1,datatype);
-            dldz_ = dLdZ; %permute(dLdZ,[3 1 2 4]);
-            dldz_upp = reshape(dldz_(1:ps,:,:,:),ps,nrows*ncols*nSamples);
-            dldz_low = reshape(dldz_(ps+1:ps+pa,:,:,:),pa,nrows*ncols*nSamples);
-            % (dVdWi)X
-            for iAngle = 1:nAnglesH
-                dW0 = genW.step(anglesW_NoDc,musW,iAngle);
-                dU0 = genU.step(anglesU,musU,iAngle);
-                a_ = X; %permute(X,[3 1 2 4]);
-                c_upp = reshape(a_(1:ceil(nDecs/2),:,:,:),ceil(nDecs/2),nrows*ncols*nSamples);
-                c_low = reshape(a_(ceil(nDecs/2)+1:nDecs,:,:,:),floor(nDecs/2),nrows*ncols*nSamples);
-                d_upp = dW0(:,1:ceil(nDecs/2))*c_upp;
-                d_low = dU0(:,1:floor(nDecs/2))*c_low;
-                expctddLdW(iAngle) = sum(dldz_upp.*d_upp,'all');
-                expctddLdW(nAnglesH+iAngle) = sum(dldz_low.*d_low,'all');
-            end
-            
-            % Instantiation of target class
-            import saivdr.dcnn.*
-            layer = nsoltInitialRotation2dLayer(...
-                'NumberOfChannels',nchs,...
-                'DecimationFactor',stride,...
-                'NoDcLeakage',true,...
-                'Name','V0');
-            layer.Mus = mus;
-            layer.Angles = [anglesW; anglesU];
-            %expctdZ = layer.predict(X);
-            
-            % Actual values
-            [actualdLdX,actualdLdW] = layer.backward(X,[],dLdZ,[]);
-            
-            % Evaluation
-            testCase.verifyInstanceOf(actualdLdX,datatype);
-            testCase.verifyInstanceOf(actualdLdW,datatype);
-            testCase.verifyThat(actualdLdX,...
-                IsEqualTo(expctddLdX,'Within',tolObj));
-            testCase.verifyThat(actualdLdW,...
-                IsEqualTo(expctddLdW,'Within',tolObj));
-            
-        end
+        # Instantiation of target class
+        layer = NsoltInitialRotation2dLayer(
+            number_of_channels=nchs,
+            decimation_factor=stride,
+            no_dc_leakage=True,
+            name='V0')
+        layer.orthTransW0.angles.data = anglesW
+        layer.orthTransW0.mus = musW
+        layer.orthTransU0.angles.data = anglesU
+        layer.orthTransU0.mus = musU
+
+        # Actual values
+        torch.autograd.set_detect_anomaly(True)
+        Z = layer.forward(X)
+        layer.zero_grad()
+        Z.backward(dLdZ)
+        actualdLdX = X.grad
+        actualdLdW_W = layer.orthTransW0.angles.grad
+        actualdLdW_U = layer.orthTransU0.angles.grad
+
+        # Evaluation
+        self.assertEqual(actualdLdX.dtype,datatype)
+        self.assertEqual(actualdLdW_W.dtype,datatype)
+        self.assertEqual(actualdLdW_U.dtype,datatype)
+        self.assertTrue(torch.allclose(actualdLdX,expctddLdX,rtol=rtol,atol=atol))
+        self.assertTrue(torch.allclose(actualdLdW_W,expctddLdW_W,rtol=rtol,atol=atol))
+        self.assertTrue(torch.allclose(actualdLdW_U,expctddLdW_U,rtol=rtol,atol=atol))
+        self.assertTrue(Z.requires_grad)
+
+    """
+    TODO: Grad check
     """
 
 if __name__ == '__main__':
