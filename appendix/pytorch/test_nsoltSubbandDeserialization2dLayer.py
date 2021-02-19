92e6f4c9a58ab4483542b428b9075386737f81de
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-02-20 07:13:54 +0900

    Added header comments

--- appendix/pytorch/test_nsoltSubbandDeserialization2dLayer.py
+++ appendix/pytorch/test_nsoltSubbandDeserialization2dLayer.py
@@ -3,8 +3,33 @@ import torch
 import torch.nn as nn
 from nsoltSubbandDeserialization2dLayer import NsoltSubbandDeserialization2dLayer
 
-class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
-
+class NsoltSubbandDeserialization2dLayerTestCase(unittest.TestCase):
+    """
+    NSOLTSUBBANDDESERIALIZATION2dLAYERTESTCASE
+    
+       １コンポーネント入力(BSSC):
+          nSamples x 1 x 1 x nElements 
+    
+       複数コンポーネント出力 (BSSC):（ツリーレベル数）
+          nSamples x nRowsLv1 x nColsLv1 x (nChsTotal-1) 
+          nSamples x nRowsLv2 x nColsLv2 x (nChsTotal-1) 
+           :
+          nSamples x nRowsLvN x nColsLvN x (nChsTotal-1) 
+          nSamples x nRowsLvN x nColsLvN x 1 
+    
+    Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+    Copyright (c) 2021, Shogo MURAMATSU
+    
+    All rights reserved.
+    
+    Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+    http://msiplab.eng.niigata-u.ac.jp/
+    """
     def testConstructor(self):
         target = NsoltSubbandDeserialization2dLayer()
         self.assertTrue(isinstance(target, nn.Module))
@@ -16,5 +41,261 @@ class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
         valueActual = target.forward(x)
         self.assertEqual(valueExpctd, valueActual)
 
+"""
+    properties (TestParameter)
+        nlevels = { 1, 2, 3 };
+        stride = { [2 2], [1 2], [2 1] };
+        nchs = { [3 3], [4 4] };
+        datatype = { 'single', 'double' };
+        nrows = struct('small', 4,'medium', 8, 'large', 16);
+        ncols = struct('small', 4,'medium', 8, 'large', 16);
+    end
+    
+    methods (TestClassTeardown)
+        function finalCheck(~)
+            import saivdr.dcnn.*
+            layer = nsoltSubbandDeserialization2dLayer(...
+                'OriginalDimension',[16 16],...
+                'NumberOfChannels',[3 3],...
+                'DecimationFactor',[2 2],...
+                'NumberOfLevels',3);
+            fprintf("\n --- Check layer for 2-D images ---\n");
+            checkLayer(layer,[424 1 1],...
+                'ObservationDimension',4,...
+                'CheckCodegenCompatibility',true)
+        end
+    end
+    
+    methods (Test)
+        
+        function testConstructor(testCase,...
+                nrows,ncols,nchs,stride,nlevels)
+            
+            % Expected values
+            height = nrows*(stride(1)^nlevels);
+            width = ncols*(stride(2)^nlevels);
+            expctdName = 'Sb_Dsz';
+            expctdDescription = "Subband deserialization " ...
+                + "(h,w) = (" ...
+                + height + "," + width + "), "  ...
+                + "lv = " ...
+                + nlevels + ", " ...
+                + "(ps,pa) = (" ...
+                + nchs(1) + "," + nchs(2) + "), "  ...
+                + "(mv,mh) = (" ...
+                + stride(1) + "," + stride(2) + ")";
+            
+            % Instantiation of target class
+            import saivdr.dcnn.*
+            layer = nsoltSubbandDeserialization2dLayer(...
+                'Name',expctdName,...
+                'OriginalDimension',[height width],...
+                'NumberOfChannels',nchs,...
+                'DecimationFactor',stride,...
+                'NumberOfLevels',nlevels);
+            
+            % Actual values
+            actualName = layer.Name;
+            actualDescription = layer.Description;
+            
+            % Evaluation
+            testCase.verifyEqual(actualName,expctdName);
+            testCase.verifyEqual(actualDescription,expctdDescription);
+        end
+        
+        function testSetOriginalDimension(testCase,...
+                nrows,ncols,nchs,stride,nlevels)
+            
+            % Expected values
+            height = nrows*(stride(1)^nlevels);
+            width = ncols*(stride(2)^nlevels);
+            
+            % Instantiation of target class
+            import saivdr.dcnn.*
+            layer = nsoltSubbandDeserialization2dLayer(...
+                'Name','Sb_Dsz',...
+                'OriginalDimension',[height width],...
+                'NumberOfChannels',nchs,...
+                'DecimationFactor',stride,...
+                'NumberOfLevels',nlevels);
+            expctdOriginalDimension = 2*[height width];
+            expctdInputSize = [2^2 1 1].*layer.InputSize;
+            
+            % Actual values
+            layer = layer.setOriginalDimension(expctdOriginalDimension);
+            actualOriginalDimension = layer.OriginalDimension;
+            actualInputSize = layer.InputSize;
+            
+            % Evaluation
+            testCase.verifyEqual(actualOriginalDimension,expctdOriginalDimension);
+            testCase.verifyEqual(actualInputSize,expctdInputSize);
+            
+        end
+        
+        function testPredict(testCase,...
+                nchs,nrows,ncols,stride,nlevels,datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            height = nrows*(stride(1)^nlevels);
+            width = ncols*(stride(2)^nlevels);
+            
+            % Parameters
+            nSamples = 8;
+            nChsTotal = sum(nchs);
+            
+            % Expected values
+            %expctdZ = cell(nlevels,1);
+            expctdZ = cell(nlevels+1,1);
+            for iLv = 1:nlevels %-1
+                subHeight = nrows * stride(1)^(nlevels-iLv);
+                subWidth = ncols * stride(2)^(nlevels-iLv);
+                expctdZ{iLv} = randn(subHeight,subWidth,nChsTotal-1,...
+                    nSamples,datatype);
+            end
+            expctdZ{nlevels+1} = randn(nrows,ncols,1,...
+                nSamples,datatype); %
+            
+            %expctdScales = zeros(nlevels,3);
+            expctdScales = zeros(nlevels+1,3);
+            %expctdScales(1,:) = [nrows ncols nChsTotal];
+            expctdScales(1,:) = [nrows ncols 1];
+            for iRevLv = 1:nlevels %2:nlevels
+                %expctdScales(iRevLv,:) = ...
+                expctdScales(iRevLv+1,:) = ...
+                    [nrows*stride(1)^(iRevLv-1) ncols*stride(2)^(iRevLv-1)  nChsTotal-1];
+            end
+            
+            % Input
+            nElements = sum(prod(expctdScales,2));
+            X = zeros(nElements,1,1,nSamples,datatype);
+            for iSample = 1:nSamples
+                x = zeros(nElements,1,datatype);
+                sidx = 0;
+                nSubElements = prod(expctdScales(1,:));
+                a = expctdZ{nlevels+1}(:,:,:,iSample);
+                x(1:nSubElements) = a(:);
+                sidx = sidx+nSubElements;
+                for iRevLv = 1:nlevels
+                    %nSubElements = prod(expctdScales(iRevLv,:));
+                    nSubElements = prod(expctdScales(iRevLv+1,:));
+                    a = expctdZ{nlevels-iRevLv+1}(:,:,:,iSample);
+                    x(sidx+1:sidx+nSubElements) = a(:);
+                    sidx = sidx+nSubElements;
+                end
+                X(:,1,1,iSample) = x;
+            end
+            expctdInputSize = [sum(prod(expctdScales,2)) 1 1 ];            
+            
+            % Instantiation of target class
+            import saivdr.dcnn.*
+            layer = nsoltSubbandDeserialization2dLayer(...
+                'Name','Sb_Dsz',...
+                'OriginalDimension',[height width],...
+                'NumberOfChannels',nchs,...
+                'DecimationFactor',stride,...
+                'NumberOfLevels',nlevels);
+            
+            % Actual values
+            %[actualZ{1:nlevels}] = layer.predict(X);
+            [actualZ{1:nlevels+1}] = layer.predict(X);
+            actualScales = layer.Scales;
+            actualInputSize = layer.InputSize;            
+            
+            % Evaluation
+            for iLv = 1:nlevels+1 % 1:nlevels
+                testCase.verifyInstanceOf(actualZ{iLv},datatype);
+                testCase.verifyThat(actualZ{iLv},...
+                    IsEqualTo(expctdZ{iLv},'Within',tolObj));
+            end
+            testCase.verifyThat(actualScales,...
+                IsEqualTo(expctdScales,'Within',tolObj));
+            testCase.verifyThat(actualInputSize,...
+                IsEqualTo(expctdInputSize,'Within',tolObj));            
+            
+        end
+        
+        function testBackward(testCase,...
+                nchs,nrows,ncols,stride,nlevels,datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            height = nrows*(stride(1)^nlevels);
+            width = ncols*(stride(2)^nlevels);
+            
+            % Parameters
+            nSamples = 8;
+            nChsTotal = sum(nchs);
+            %dLdZ = cell(nlevels,1);
+            dLdZ = cell(nlevels+1,1);
+            for iLv = 1:nlevels % -1
+                subHeight = nrows * stride(1)^(nlevels-iLv);
+                subWidth = ncols * stride(2)^(nlevels-iLv);
+                dLdZ{iLv} = randn(subHeight,subWidth,nChsTotal-1,...
+                    nSamples,datatype);
+            end
+            dLdZ{nlevels+1} = randn(nrows,ncols,1,...
+                nSamples,datatype);
+            
+            % Expected values
+            %expctdScales = zeros(nlevels,3);
+            expctdScales = zeros(nlevels+1,3);
+            %expctdScales(1,:) = [nrows ncols nChsTotal];
+            expctdScales(1,:) = [nrows ncols 1];
+            for iRevLv = 1:nlevels % 2:nlevels
+                %expctdScales(iRevLv,:) = ...
+                expctdScales(iRevLv+1,:) = ...
+                    [nrows*stride(1)^(iRevLv-1) ncols*stride(2)^(iRevLv-1)  nChsTotal-1];
+            end
+            nElements = sum(prod(expctdScales,2));
+            expctddLdX = zeros(nElements,1,1,nSamples,datatype);
+            for iSample = 1:nSamples
+                x = zeros(nElements,1,datatype);
+                sidx = 0;
+                nSubElements = prod(expctdScales(1,:));                
+                a = dLdZ{nlevels+1}(:,:,:,iSample);
+                x(1:nSubElements) = a(:);
+                sidx = sidx+nSubElements;
+                for iRevLv = 1:nlevels
+                    nSubElements = prod(expctdScales(iRevLv+1,:));
+                    a = dLdZ{nlevels-iRevLv+1}(:,:,:,iSample);
+                    x(sidx+1:sidx+nSubElements) = a(:);
+                    sidx = sidx+nSubElements;
+                end
+                expctddLdX(:,1,1,iSample) = x;
+            end
+            
+            % Instantiation of target class
+            import saivdr.dcnn.*
+            layer = nsoltSubbandDeserialization2dLayer(...
+                'Name','Sb_Srz',...
+                'OriginalDimension',[height width],...
+                'NumberOfChannels',nchs,...
+                'DecimationFactor',stride,...
+                'NumberOfLevels',nlevels);
+            
+            % Actual values
+            %args = cell(1,1+nlevels+nlevels+1);
+            args = cell(1,1+nlevels+1+nlevels+1+1);
+            for iLv = 1:nlevels+1 %1:nlevels
+                %args{1+nlevels+iLv} = dLdZ{iLv};
+                args{1+nlevels+1+iLv} = dLdZ{iLv};
+            end
+            actualdLdX = layer.backward(args{:});
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualdLdX,datatype);
+            testCase.verifyThat(actualdLdX,...
+                IsEqualTo(expctddLdX,'Within',tolObj));
+            
+        end
+    end
+    
+end
+"""
 if __name__ == '__main__':
     unittest.main()
--- appendix/pytorch/test_nsoltSubbandSerialization2dLayer.py
+++ appendix/pytorch/test_nsoltSubbandSerialization2dLayer.py
@@ -3,8 +3,33 @@ import torch
 import torch.nn as nn
 from nsoltSubbandSerialization2dLayer import NsoltSubbandSerialization2dLayer
 
-class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
-
+class NsoltSubbandSerialization2dLayerTestCase(unittest.TestCase):
+    """
+    NSOLTSUBBANDSERIALIZATION2DLAYERTESTCASE
+    
+       複数コンポーネント入力 (BSSC):（ツリーレベル数）
+          nSamples x nRowsLv1 x nColsLv1 x (nChsTotal-1) 
+          nSamples x nRowsLv2 x nColsLv2 x (nChsTotal-1) 
+           :
+          nSamples x nRowsLvN x nColsLvN x (nChsTotal-1) 
+          nSamples x nRowsLvN x nColsLvN x 1 
+    
+       １コンポーネント出力(BSSC):
+          nSamples x 1 x 1 x nElements
+    
+    Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+    Copyright (c) 2021, Shogo MURAMATSU
+    
+    All rights reserved.
+    
+    Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+    http://msiplab.eng.niigata-u.ac.jp/
+    """
     def testConstructor(self):
         target = NsoltSubbandSerialization2dLayer()
         self.assertTrue(isinstance(target, nn.Module))
@@ -15,6 +40,255 @@ class NsoltAtomExtention2dLayerTestCase(unittest.TestCase):
         valueExpctd = 0.0
         valueActual = target.forward(x)
         self.assertEqual(valueExpctd, valueActual)
+
+"""
+    properties (TestParameter)
+        nlevels = { 1, 2, 3 };
+        stride = { [2 2], [1 2], [2 1] };
+        nchs = { [3 3], [4 4] };
+        datatype = { 'single', 'double' };
+        nrows = struct('small', 4,'medium', 8, 'large', 16);
+        ncols = struct('small', 4,'medium', 8, 'large', 16);
+    end
+    
+    methods (TestClassTeardown)
+        function finalCheck(~)
+            import saivdr.dcnn.*
+            layer = nsoltSubbandSerialization2dLayer(...
+                'OriginalDimension',[16 16],...
+                'NumberOfChannels',[3 3],...
+                'DecimationFactor',[2 2],...
+                'NumberOfLevels',3);
+            fprintf("\n --- Check layer for 2-D images ---\n");
+            checkLayer(layer,{[8 8 5], [4 4 5], [2 2 5], [2 2 1]},...
+                'ObservationDimension',4,...
+                'CheckCodegenCompatibility',true)
+        end
+    end
+    
+    methods (Test)
+        
+        function testConstructor(testCase,...
+                nrows,ncols,nchs,stride,nlevels)
+            
+            % Expected values
+            height = nrows*(stride(1)^nlevels);
+            width = ncols*(stride(2)^nlevels);
+            expctdName = 'Sb_Srz';
+            expctdDescription = "Subband serialization " ...
+                + "(h,w) = (" ...
+                + height + "," + width + "), "  ...
+                + "lv = " ...
+                + nlevels + ", " ...
+                + "(ps,pa) = (" ...
+                + nchs(1) + "," + nchs(2) + "), "  ...
+                + "(mv,mh) = (" ...
+                + stride(1) + "," + stride(2) + ")";
+            
+            % Instantiation of target class
+            import saivdr.dcnn.*
+            layer = nsoltSubbandSerialization2dLayer(...
+                'Name',expctdName,...
+                'OriginalDimension',[height width],...
+                'NumberOfChannels',nchs,...
+                'DecimationFactor',stride,...
+                'NumberOfLevels',nlevels);
+            
+            % Actual values
+            actualName = layer.Name;
+            actualDescription = layer.Description;
+            
+            % Evaluation
+            testCase.verifyEqual(actualName,expctdName);
+            testCase.verifyEqual(actualDescription,expctdDescription);
+        end
+        
+        function testSetOriginalDimension(testCase,...
+                nrows,ncols,nchs,stride,nlevels)
+            
+            % Expected values
+            height = nrows*(stride(1)^nlevels);
+            width = ncols*(stride(2)^nlevels);
+            
+            % Instantiation of target class
+            import saivdr.dcnn.*
+            layer = nsoltSubbandSerialization2dLayer(...
+                'Name','Sb_Srz',...
+                'OriginalDimension',[height width],...
+                'NumberOfChannels',nchs,...
+                'DecimationFactor',stride,...
+                'NumberOfLevels',nlevels);
+            expctdOriginalDimension = 2*[height width];
+            expctdScales = [2 2 1].*layer.Scales;
+            
+            % Actual values
+            layer = layer.setOriginalDimension(expctdOriginalDimension);
+            actualOriginalDimension = layer.OriginalDimension;
+            actualScales = layer.Scales;
+            
+            % Evaluation
+            testCase.verifyEqual(actualOriginalDimension,expctdOriginalDimension);
+            testCase.verifyEqual(actualScales,expctdScales);
+            
+        end
+        
+        
+        function testPredict(testCase,...
+                nchs,nrows,ncols,stride,nlevels,datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            height = nrows*(stride(1)^nlevels);
+            width = ncols*(stride(2)^nlevels);
+            
+            % Parameters
+            nSamples = 8;
+            nChsTotal = sum(nchs);
+            %X = cell(nlevels,1);
+            X = cell(nlevels+1,1);
+            for iLv = 1:nlevels %-1
+                subHeight = nrows * stride(1)^(nlevels-iLv);
+                subWidth = ncols * stride(2)^(nlevels-iLv);
+                X{iLv} = randn(subHeight,subWidth,nChsTotal-1,...
+                    nSamples,datatype);
+            end
+            X{nlevels+1} = randn(nrows,ncols,1,...
+                nSamples,datatype);
+            
+            % Expected values
+            %expctdScales = zeros(nlevels,3);
+            expctdScales = zeros(nlevels+1,3);
+            %expctdScales(1,:) = [nrows ncols nChsTotal];
+            expctdScales(1,:) = [nrows ncols 1];
+            for iRevLv = 1:nlevels %2:nlevels
+                expctdScales(iRevLv+1,:) = ...
+                    [nrows*stride(1)^(iRevLv-1) ncols*stride(2)^(iRevLv-1)  nChsTotal-1];
+            end
+            nElements = sum(prod(expctdScales,2));
+            expctdZ = zeros(nElements,1,1,nSamples,datatype);
+            for iSample = 1:nSamples
+                x = zeros(nElements,1,datatype);
+                sidx = 0;
+                nSubElements = prod(expctdScales(1,:));                
+                a = X{nlevels+1}(:,:,:,iSample);                
+                x(1:nSubElements) = a(:);                
+                sidx = sidx+nSubElements;                
+                for iRevLv = 1:nlevels
+                    nSubElements = prod(expctdScales(iRevLv+1,:));
+                    a = X{nlevels-iRevLv+1}(:,:,:,iSample);
+                    x(sidx+1:sidx+nSubElements) = a(:);
+                    sidx = sidx+nSubElements;
+                end
+                expctdZ(:,1,1,iSample) = x;
+            end
+            
+            % Instantiation of target class
+            import saivdr.dcnn.*
+            layer = nsoltSubbandSerialization2dLayer(...
+                'Name','Sb_Srz',...
+                'OriginalDimension',[height width],...
+                'NumberOfChannels',nchs,...
+                'DecimationFactor',stride,...
+                'NumberOfLevels',nlevels);
+            
+            % Actual values
+            actualZ = layer.predict(X{:});
+            actualScales = layer.Scales;
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualZ,datatype);
+            testCase.verifyThat(actualZ,...
+                IsEqualTo(expctdZ,'Within',tolObj));
+            testCase.verifyThat(actualScales,...
+                IsEqualTo(expctdScales,'Within',tolObj));
+            
+        end
+        
+        function testBackward(testCase,...
+                nchs,nrows,ncols,stride,nlevels,datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            height = nrows*(stride(1)^nlevels);
+            width = ncols*(stride(2)^nlevels);
+            
+            % Parameters
+            nSamples = 8;
+            nChsTotal = sum(nchs);
+            
+            % Expected values
+            expctddLdX = cell(nlevels+1,1);
+            for iLv = 1:nlevels %-1
+                subHeight = nrows * stride(1)^(nlevels-iLv);
+                subWidth = ncols * stride(2)^(nlevels-iLv);
+                expctddLdX{iLv} = randn(subHeight,subWidth,nChsTotal-1,...
+                    nSamples,datatype);
+            end
+            expctddLdX{nlevels+1} = randn(nrows,ncols,1,...
+                nSamples,datatype); %
+            
+            %expctdScales = zeros(nlevels,3);
+            expctdScales = zeros(nlevels+1,3);
+            %expctdScales(1,:) = [nrows ncols nChsTotal];
+            expctdScales(1,:) = [nrows ncols 1];
+            for iRevLv = 1:nlevels %2:nlevels
+                expctdScales(iRevLv+1,:) = ...
+                    [nrows*stride(1)^(iRevLv-1) ncols*stride(2)^(iRevLv-1)  nChsTotal-1];
+            end
+            
+            % Input
+            nElements = sum(prod(expctdScales,2));
+            dLdZ = zeros(nElements,1,1,nSamples,datatype);
+            for iSample = 1:nSamples
+                x = zeros(nElements,1,datatype);
+                sidx = 0;
+                nSubElements = prod(expctdScales(1,:));                
+                a = expctddLdX{nlevels+1}(:,:,:,iSample);
+                x(1:nSubElements) = a(:);
+                sidx = sidx+nSubElements;                
+                for iRevLv = 1:nlevels
+                    %nSubElements = prod(expctdScales(iRevLv,:));
+                    nSubElements = prod(expctdScales(iRevLv+1,:));
+                    a = expctddLdX{nlevels-iRevLv+1}(:,:,:,iSample);
+                    x(sidx+1:sidx+nSubElements) = a(:);
+                    sidx = sidx+nSubElements;
+                end
+                dLdZ(:,1,1,iSample) = x;
+            end
+            
+            % Instantiation of target class
+            import saivdr.dcnn.*
+            layer = nsoltSubbandSerialization2dLayer(...
+                'Name','Sb_Srz',...
+                'OriginalDimension',[height width],...
+                'NumberOfChannels',nchs,...
+                'DecimationFactor',stride,...
+                'NumberOfLevels',nlevels);
+            
+            % Actual values
+            %args = cell(1,nlevels+1+1+1);
+            args = cell(1,nlevels+2+1+1);
+            %args{nlevels+2} = dLdZ;
+            args{nlevels+3} = dLdZ;
+            %[actualdLdX{1:nlevels}] = layer.backward(args{:});
+            [actualdLdX{1:nlevels+1}] = layer.backward(args{:});
+            
+            % Evaluation
+            for iLv = 1:nlevels+1 %1:nlevels
+                testCase.verifyInstanceOf(actualdLdX{iLv},datatype);
+                testCase.verifyThat(actualdLdX{iLv},...
+                    IsEqualTo(expctddLdX{iLv},'Within',tolObj));
+            end
+            
+        end
+    end
+    
+end
+"""
 
 if __name__ == '__main__':
     unittest.main()
