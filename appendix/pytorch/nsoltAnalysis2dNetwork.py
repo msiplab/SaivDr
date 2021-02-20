f54bf29ef107916b26f42c0fbd6a23f2ef73b2b7
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-02-20 15:11:04 +0900

    Added test cases for the construction

--- appendix/pytorch/nsoltAnalysis2dNetwork.py
+++ appendix/pytorch/nsoltAnalysis2dNetwork.py
@@ -2,9 +2,28 @@ import torch
 import torch.nn as nn
 
 class NsoltAnalysis2dNetwork(nn.Module):
-
-    def __init__(self):
+    """
+    NSOLTANALYSIS2DNETWORK
+    
+    Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+    Copyright (c) 2021, Yasas Dulanjaya and Shogo MURAMATSU
+    
+    All rights reserved.
+    
+    Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+        http://msiplab.eng.niigata-u.ac.jp/ 
+    """
+    def __init__(self,
+        number_of_channels=[],
+        decimation_factor=[]):
         super(NsoltAnalysis2dNetwork, self).__init__()
+        self.number_of_channels = number_of_channels
+        self.decimation_factor = decimation_factor
 
     def forward(self,x):
         return x
