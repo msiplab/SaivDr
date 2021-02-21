a1883b1c00d057fdc97601bf77462f85ddca87b5
yasas99 <45630392+yasas99@users.noreply.github.com>
2021-02-21 21:09:01 +0900

    Update nsoltSynthesis2dNetwork.py

--- appendix/pytorch/nsoltSynthesis2dNetwork.py
+++ appendix/pytorch/nsoltSynthesis2dNetwork.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+from nsoltBlockIdct2dLayer import NsoltBlockIdct2dLayer
+from nsoltFinalRotation2dLayer import NsoltFinalRotation2dLayer 
+
 
 class NsoltSynthesis2dNetwork(nn.Module):
 
@@ -9,6 +12,19 @@ class NsoltSynthesis2dNetwork(nn.Module):
         super(NsoltSynthesis2dNetwork, self).__init__()
         self.number_of_channels = number_of_channels
         self.decimation_factor = decimation_factor
+        
+        # Instantiation of layers
+        self.layerV0 = NsoltFinalRotation2dLayer(
+            number_of_channels=number_of_channels,
+            decimation_factor=decimation_factor,
+            name='V0'
+        )
+        self.layerE0 = NsoltBlockIdct2dLayer(
+            decimation_factor=decimation_factor,
+            name='E0'
+        )
 
     def forward(self,x):
-        return x
+        u = self.layerV0.forward(x)
+        y = self.layerE0.forward(u) 
+        return y
