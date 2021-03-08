fd24f387864ab80b96636b1161b33d2e689ce0ce
yasas99 <45630392+yasas99@users.noreply.github.com>
2021-03-08 19:15:21 +0900

    Update nsoltSynthesis2dNetwork.py

--- appendix/pytorch/nsoltSynthesis2dNetwork.py
+++ appendix/pytorch/nsoltSynthesis2dNetwork.py
@@ -8,7 +8,22 @@ from nsoltLayerExceptions import InvalidNumberOfChannels, InvalidPolyPhaseOrder,
 from nsoltUtility import Direction
 
 class NsoltSynthesis2dNetwork(nn.Module):
-
+    """
+    NSOLTSYNTHESIS2DNETWORK
+    
+    Requirements: Python 3.7.x, PyTorch 1.7.x
+    
+    Copyright (c) 2021, Yasas Dulanjaya, Shogo MURAMATSU
+    
+    All rights reserved.
+    
+    Contact address: Shogo MURAMATSU,
+        Faculty of Engineering, Niigata University,
+        8050 2-no-cho Ikarashi, Nishi-ku,
+        Niigata, 950-2181, JAPAN
+    
+        http://msiplab.eng.niigata-u.ac.jp/ 
+    """
     def __init__(self,
         number_of_channels=[],
         decimation_factor=[],
@@ -20,7 +35,7 @@ class NsoltSynthesis2dNetwork(nn.Module):
         # # of channels
         if number_of_channels[0] != number_of_channels[1]:
             raise InvalidNumberOfChannels(
-                    '[%d %d] : Currently, Type-I NSOLT is only suported, where the symmetric and antisymmetric channel numbers should be the same.'\
+                    '[%d, %d] : Currently, Type-I NSOLT is only suported, where the symmetric and antisymmetric channel numbers should be the same.'\
                     %(number_of_channels[0],number_of_channels[1]))
         self.number_of_channels = number_of_channels
         
@@ -42,52 +57,51 @@ class NsoltSynthesis2dNetwork(nn.Module):
                         %(number_of_vanishing_moments))
         self.number_of_vanishing_moments = number_of_vanishing_moments
         
-        
         # Instantiation of layers
         self.layers = nn.Sequential()
         
         # Vertical concatenation
-        for iOrderV in range(2,polyphase_order[Direction.VERTICAL]+1,2):            
-            self.layers.add_module('Vv~%d'%(iOrderV-1),NsoltIntermediateRotation2dLayer(
-                number_of_channels=number_of_channels,
-                mode='Synthesis',
-                mus=-1))
-            self.layers.add_module('Qv~%dus'%(iOrderV-1),NsoltAtomExtension2dLayer(
-                number_of_channels=number_of_channels,
-                direction='Down',
-                target_channels='Sum'))
+        for iOrderV in range(polyphase_order[Direction.VERTICAL],1,-2):            
             self.layers.add_module('Vv~%d'%(iOrderV),NsoltIntermediateRotation2dLayer(
                 number_of_channels=number_of_channels,
                 mode='Synthesis',
                 mus=-1))
-            self.layers.add_module('Qv~%ddd'%iOrderV,NsoltAtomExtension2dLayer(
+            self.layers.add_module('Qv~%dus'%(iOrderV),NsoltAtomExtension2dLayer(
+                number_of_channels=number_of_channels,
+                direction='Down',
+                target_channels='Sum'))
+            self.layers.add_module('Vv~%d'%(iOrderV-1),NsoltIntermediateRotation2dLayer(
+                number_of_channels=number_of_channels,
+                mode='Synthesis',
+                mus=-1))
+            self.layers.add_module('Qv~%ddd'%(iOrderV-1),NsoltAtomExtension2dLayer(
                 number_of_channels=number_of_channels,
                 direction='Up',
                 target_channels='Difference'))
         
         # Horizontal concatenation
-        for iOrderH in range(2,polyphase_order[Direction.HORIZONTAL]+1,2):
-            
+        for iOrderH in range(polyphase_order[Direction.HORIZONTAL],1,-2):
+            self.layers.add_module('Vh~%d'%(iOrderH),NsoltIntermediateRotation2dLayer(
+                number_of_channels=number_of_channels,
+                mode='Synthesis',
+                mus=-1))
+            self.layers.add_module('Qh~%dls'%(iOrderH),NsoltAtomExtension2dLayer(
+                number_of_channels=number_of_channels,
+                direction='Right',
+                target_channels='Sum'))
             self.layers.add_module('Vh~%d'%(iOrderH-1),NsoltIntermediateRotation2dLayer(
                 number_of_channels=number_of_channels,
                 mode='Synthesis',
                 mus=-1))
-            self.layers.add_module('Qh~%dls'%(iOrderH-1),NsoltAtomExtension2dLayer(
-                number_of_channels=number_of_channels,
-                direction='Right',
-                target_channels='Sum'))
-            self.layers.add_module('Vh~%d'%iOrderH,NsoltIntermediateRotation2dLayer(
-                number_of_channels=number_of_channels,
-                mode='Synthesis',
-                mus=-1))
-            self.layers.add_module('Qh~%drd'%iOrderH,NsoltAtomExtension2dLayer(
+            self.layers.add_module('Qh~%drd'%(iOrderH-1),NsoltAtomExtension2dLayer(
                 number_of_channels=number_of_channels,
                 direction='Left',
                 target_channels='Difference'))
             
         self.layers.add_module('V0~',NsoltFinalRotation2dLayer(
             number_of_channels=number_of_channels,
-            decimation_factor=decimation_factor))
+            decimation_factor=decimation_factor,
+            no_dc_leakage=(self.number_of_vanishing_moments==1)))
         self.layers.add_module('E0~',NsoltBlockIdct2dLayer(
             decimation_factor=decimation_factor))    
             
