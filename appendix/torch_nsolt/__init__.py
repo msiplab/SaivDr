b4eebb47e4ef135f6467584371afea57b2e5fc46
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-03-21 14:32:20 +0900

    Revised the name space initialization

---
+++ appendix/torch_nsolt/__init__.py
@@ -0,0 +1,36 @@
+__all__ = [
+    'nsoltAnalysis2dNetwork',
+    'nsoltAtomExtension2dLayer',
+    'nsoltBlockDct2dLayer',
+    'nsoltBlockDct3dLayer',
+    'nsoltBlockIdct2dLayer',
+    'nsoltBlockIdct3dLayer',
+    'nsoltChannelConcatenation2dLayer',
+    'nsoltChannelSeparation2dLayer',
+    'nsoltFinalRotation2dLayer',
+    'nsoltInitialRotation2dLayer',
+    'nsoltIntermediateRotation2dLayer',
+    'nsoltLayerExceptions',
+    'nsoltSubbandDeserialization2dLayer',
+    'nsoltSubbandSerialization2dLayer',
+    'nsoltSynthesis2dNetwork',
+    'nsoltUtility',
+    'orthonormalTransform'
+]
+from nsoltAnalysis2dNetwork import *
+from nsoltAtomExtension2dLayer import *
+from nsoltBlockDct2dLayer import *
+from nsoltBlockDct3dLayer import *
+from nsoltBlockIdct2dLayer import *
+from nsoltBlockIdct3dLayer import *
+from nsoltChannelConcatenation2dLayer import *
+from nsoltChannelSeparation2dLayer import *
+from nsoltFinalRotation2dLayer import *
+from nsoltInitialRotation2dLayer import *
+from nsoltIntermediateRotation2dLayer import *
+from nsoltLayerExceptions import *
+from nsoltSubbandDeserialization2dLayer import *
+from nsoltSubbandSerialization2dLayer import *
+from nsoltSynthesis2dNetwork import *
+from nsoltUtility import *
+from orthonormalTransform import *
