b5549f391db67f05106f3231f6227bf1d5fdcc57
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-04-03 20:34:07 +0900

    Revised to make the parameer Lambda in soft-thresholding learnable

--- examples/dcnn/sfthLayer.m
+++ examples/dcnn/sfthLayer.m
@@ -1,7 +1,11 @@
 classdef sfthLayer < nnet.layer.Layer 
     % Example custom Soft-thresholding layer.
 
-    properties %(Learnable)
+    properties
+        NumberOfChannels
+    end
+    
+    properties(Learnable)
         % Layer learnable parameters
         % Scaling coefficient
         Lambda
@@ -12,16 +16,18 @@ classdef sfthLayer < nnet.layer.Layer
             p = inputParser;
             addParameter(p,'Name','')
             addParameter(p,'Lambda',0)
+            addParameter(p,'NumberOfChannels',1)
             parse(p,varargin{:})
             
             % Set layer name.
             layer.Name = p.Results.Name;
-
+            layer.NumberOfChannels = p.Results.NumberOfChannels;
+            
             % Initialize scaling coefficient.
-            layer.Lambda = p.Results.Lambda;            
+            layer.Lambda = permute(p.Results.Lambda.*ones(layer.NumberOfChannels,1),[2 3 1]);            
             
             % Set layer description.
-            layer.Description = "Soft-thresholding with Lamda = " + layer.Lambda;
+            layer.Description = "Soft-thresholding for " + layer.NumberOfChannels + " channes";
         end
         
         function Z = predict(layer, X)
