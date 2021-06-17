f8fc1cf580aa5256aac5586c1e5530ec6909f18f
shodimaggio <shogo@eng.niigata-u.ac.jp>
2021-06-18 07:21:38 +0900

    Added measuement process layers

---
+++ examples/dcnn/adjointMeasurement2dLayer.m
@@ -0,0 +1,90 @@
+classdef adjointMeasurement2dLayer < nnet.layer.Layer
+    % adjointMeasurment2dLayer
+    %
+    % SSCB -> SSCB
+    %
+    % Copyright (c) Shogo MURAMATSU, 2021
+    % All rights reserved.
+    %
+    properties
+        Sigma
+        PsfSize
+        PadOption
+        DecimationFactor
+    end
+    
+    properties
+
+    end
+    
+    properties (Access=private, Hidden)
+        psf
+    end
+    
+    methods
+        function layer = adjointMeasurement2dLayer(varargin)
+            p = inputParser;
+            addParameter(p,'Name','')
+            addParameter(p,'Sigma',2)
+            addParameter(p,'PsfSize',9);
+            addParameter(p,'PadOption','Zeros');
+            addParameter(p,'DecimationFactor',1);
+            parse(p,varargin{:})
+            
+            % Set layer name.
+            layer.Name = p.Results.Name;
+            layer.Sigma = p.Results.Sigma;
+            layer.PadOption = p.Results.PadOption;
+            layer.PsfSize = p.Results.PsfSize;
+            layer.DecimationFactor = p.Results.DecimationFactor;
+            
+            %
+            if isscalar(layer.PsfSize)
+                layer.PsfSize = [1 1]*layer.PsfSize;
+            end
+            if isscalar(layer.DecimationFactor)
+                layer.DecimationFactor = [1 1]*layer.DecimationFactor;
+            end
+            
+            %
+            layer.psf = fspecial('gaussian',...
+                layer.PsfSize,layer.Sigma);
+            
+            % Set layer description.
+            layer.Description = "Adjoint of measurement process " + ...
+                "PsfSize: [" + layer.PsfSize(1) + " " + layer.PsfSize(2) + "], " + ...
+                "PadOption: " + layer.PadOption + ", " + ...
+                "Stride: [" + layer.DecimationFactor(1) + " " + layer.DecimationFactor(2) + "])";
+        end
+        
+        function Z = predict(layer, X)
+            padopt = layer.PadOption;
+            decfactor = layer.DecimationFactor;
+            upsample2 = @(x,m) ipermute(upsample(permute(upsample(x,m(1)),[2 1 3 4]),m(2)),[2 1 3 4]);
+            szBatch = size(X,4);
+            Y = upsample2(X,decfactor);
+            Z = zeros(size(Y),'like',X);
+            for idx = 1:szBatch
+                y = Y(:,:,:,idx);
+                Z(:,:,:,idx) = imfilter(y,layer.psf,'corr',padopt);
+            end
+        end        
+        
+        function dLdX = backward(layer,~,~,dLdZ,~)
+            padopt = layer.PadOption;
+            decfactor = layer.DecimationFactor;
+            downsample2 = @(x,m) ipermute(downsample(permute(downsample(x,m(1)),[2 1 3 4]),m(2)),[2 1 3 4]);
+            szBatch = size(dLdZ,4);
+            dldy = zeros(size(dLdZ),'like',dLdZ);
+            for idx = 1:szBatch
+                dldz = dLdZ(:,:,:,idx);
+                dldy(:,:,:,idx) = imfilter(dldz,layer.psf,'conv',padopt);
+            end
+            dLdX = downsample2(dldy,decfactor);
+        end
+        
+
+        
+    end
+    
+end
---
+++ examples/dcnn/forwardMeasurement2dLayer.m
@@ -0,0 +1,91 @@
+classdef forwardMeasurement2dLayer < nnet.layer.Layer
+    % forwardMeasurment2dLayer
+    %
+    % SSCB -> SSCB
+    %
+    % Copyright (c) Shogo MURAMATSU, 2021
+    % All rights reserved.
+    %
+    properties
+        Sigma
+        PsfSize
+        PadOption
+        DecimationFactor
+    end
+    
+    properties
+
+    end
+    
+    properties (Access=private, Hidden)
+        psf
+    end
+    
+    methods
+        function layer = forwardMeasurement2dLayer(varargin)
+            p = inputParser;
+            addParameter(p,'Name','')
+            addParameter(p,'Sigma',2)
+            addParameter(p,'PsfSize',9);
+            addParameter(p,'PadOption','Zeros');
+            addParameter(p,'DecimationFactor',1);
+            parse(p,varargin{:})
+            
+            % Set layer name.
+            layer.Name = p.Results.Name;
+            layer.Sigma = p.Results.Sigma;
+            layer.PadOption = p.Results.PadOption;
+            layer.PsfSize = p.Results.PsfSize;
+            layer.DecimationFactor = p.Results.DecimationFactor;
+            
+            %
+            if isscalar(layer.PsfSize)
+                layer.PsfSize = [1 1]*layer.PsfSize;
+            end
+            if isscalar(layer.DecimationFactor)
+                layer.DecimationFactor = [1 1]*layer.DecimationFactor;
+            end
+            
+            %
+            layer.psf = fspecial('gaussian',...
+                layer.PsfSize,layer.Sigma);
+            
+            % Set layer description.
+            layer.Description = "Forward measurement process " + ...
+                "PsfSize: [" + layer.PsfSize(1) + " " + layer.PsfSize(2) + "], " + ...
+                "PadOption: " + layer.PadOption + ", " + ...
+                "Stride: [" + layer.DecimationFactor(1) + " " + layer.DecimationFactor(2) + "])";
+        end
+        
+        function Z = predict(layer, X)
+            padopt = layer.PadOption;
+            decfactor = layer.DecimationFactor;
+            downsample2 = @(x,m) ipermute(downsample(permute(downsample(x,m(1)),[2 1 3 4]),m(2)),[2 1 3 4]);
+            szBatch = size(X,4);
+            if any(mod([size(X,1) size(X,2)],decfactor))
+                warning('The image size is not a multiple of decimation factor.');
+            end
+            y = zeros(size(X),'like',X);
+            for idx = 1:szBatch
+                x = X(:,:,:,idx);
+                y(:,:,:,idx) = imfilter(x,layer.psf,'conv',padopt);
+            end
+            Z = downsample2(y,decfactor);
+        end
+        
+        function dLdX = backward(layer,X,~,dLdZ,~)
+            padopt = layer.PadOption;
+            decfactor = layer.DecimationFactor;
+            upsample2 = @(x,m) ipermute(upsample(permute(upsample(x,m(1)),[2 1 3 4]),m(2)),[2 1 3 4]);
+            szBatch = size(dLdZ,4);
+            dLdY = upsample2(dLdZ,decfactor);
+            dLdX = zeros(size(X),'like',dLdZ);
+            for idx = 1:szBatch
+                dldy = dLdY(:,:,:,idx);
+                dLdX(:,:,:,idx) = imfilter(dldy,layer.psf,'corr',padopt);
+            end
+        end
+        
+    end
+    
+end
